c720f57aa4b8ed03fb79173711bfa8dc3a368655
Kevin Kamto <kamtokevin@gmail.com>
2021-03-30 07:10:31 -0400

    feat: list of athletes + appbar

--- lib/MyAthletes.dart
+++ lib/MyAthletes.dart
@@ -1,5 +1,6 @@
 //To be Implemented -- keep track of bets
 
+import 'package:ae_dapp/Athlete.dart';
 import 'package:flutter/material.dart';
 
 class MyAthletes extends StatefulWidget {
@@ -10,10 +11,45 @@ class MyAthletes extends StatefulWidget {
 }
 
 class _MyAthletesState extends State<MyAthletes> {
+  List<Athlete> _MyAthletes = List.generate(20, (index) => Athlete(name: "A", fantasyPoints: (11.4*index), playerID: index*1000,));
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: ,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Athletes"),
+        actions: [],
+      ),
+body: ReorderableListView.builder(
+          itemCount: _MyAthletes.length,
+          itemBuilder: (context, index) {
+            final Athlete productName = _MyAthletes[index];
+            return Card(
+              key: ValueKey(productName),
+              color: Colors.amberAccent,
+              elevation: 1,
+              margin: const EdgeInsets.all(10),
+              child: ListTile(
+                contentPadding: EdgeInsets.all(25),
+                title: Text(
+                  productName.name,
+                  style: TextStyle(fontSize: 18),
+                ),
+                trailing: Icon(Icons.drag_handle),
+                onTap: () {/* Do something else */},
+              ),
+            );
+          },
+          // The reorder function
+          onReorder: (oldIndex, newIndex) {
+            setState(() {
+              if (newIndex > oldIndex) {
+                newIndex = newIndex - 1;
+              }
+              final element = _MyAthletes.removeAt(oldIndex);
+              _MyAthletes.insert(newIndex, element);
+            });
+          }),
     );
   }
 }
