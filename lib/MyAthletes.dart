f47f1a0414ec3e22af02038bd339767f9f9ea0c3
Kevin Kamto <kamtokevin@gmail.com>
2021-03-30 07:00:41 -0400

    feat: broke bread on ownable athletes

---
+++ lib/MyAthletes.dart
@@ -0,0 +1,19 @@
+//To be Implemented -- keep track of bets
+
+import 'package:flutter/material.dart';
+
+class MyAthletes extends StatefulWidget {
+  MyAthletes({Key key}) : super(key: key);
+
+  @override
+  _MyAthletesState createState() => _MyAthletesState();
+}
+
+class _MyAthletesState extends State<MyAthletes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: ,
+    );
+  }
+}
