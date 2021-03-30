00d56cea469e56ba9c2c1b1a993e719b6641966f
Kevin Kamto <kamtokevin@gmail.com>
2021-03-30 06:39:09 -0400

    feat: basic smart contracts linked to UI

--- lib/contract_linking.dart
+++ lib/contract_linking.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart';
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 const HelloWorld = artifacts.require("HelloWorld")
-
+const AECoin = artifacts.require("AECoin");
 module.exports = function (deployer) {
+    deployer.deploy(AECoin);
     deployer.deploy(HelloWorld);
 }
