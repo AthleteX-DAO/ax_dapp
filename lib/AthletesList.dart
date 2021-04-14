639405b92dffc0706f475433f15a991c222ff208
Kevin Kamto <kamtokevin@gmail.com>
2021-04-14 19:40:03 -0400

    restor: broken search

--- lib/AthletesList.dart
+++ lib/AthletesList.dart
@@ -8,12 +8,7 @@ class AthletesList extends StatefulWidget {
 }
 
 class _AthletesListState extends State<AthletesList> {
-  final _athletesList = <Athlete>[]; //All athletes
-  final TextEditingController _filter = new TextEditingController(); // 
-  Widget _appBarTitle = new Text( 'Buy an Athlete' );
-  String _searchQuery = "";
-  List filteredNames = <Athlete>[]; // names filtered by search text
-  Icon _searchIcon = new Icon(Icons.search); 
+  final _athletesList = <Athlete>[];
   final _boughtAthletes = <Athlete>{};
 
   @override
@@ -22,53 +17,29 @@ class _AthletesListState extends State<AthletesList> {
     super.initState();
   }
 
-  // HTTP requests for athletes
-  Future<List<Athlete>> _loadData() async {
-    return fetchAthletes(); // From Athlete class
+  Future<void> _loadData() async {
+    fetchAthletes();
   }
 
-  void _searchPressed() {
-  setState(() {
-    if (this._searchIcon.icon == Icons.search) {
-      this._searchIcon = new Icon(Icons.close);
-      this._appBarTitle = new TextField(
-        controller: _filter,
-        decoration: new InputDecoration(
-          suffixIcon: new Icon(Icons.search),
-          hintText: 'Search...',
-        ),
-      );
-    } else {
-      this._searchIcon = new Icon(Icons.search);
-      this._appBarTitle = new Text('Search Example');
-      filteredNames = _athletesList;
-      _filter.clear();
-    }
-  });
-}
-
   Widget _buildAthletes(AsyncSnapshot<List<Athlete>> snapshot) {
-    for (int i = 0; i < snapshot.data.length; i++)
+    return ListView.builder(
+      itemCount: snapshot.data.length,
+      padding: EdgeInsets.all(8),
+      itemBuilder: (context, index) {
+        if (index.isOdd) return Divider(); /*2*/
 
-      return ListView.builder(
-        itemCount: snapshot.data.length,
-        padding: EdgeInsets.all(8),
-        itemBuilder: (context, index) {
-          if (index.isOdd) return Divider(); /*2*/
+        final i = index ~/ 2; /*3*/
+        if (i >= _athletesList.length) {
+          _athletesList.addAll(snapshot.data.take(7));
+        }
 
-          final i = index ~/ 2; /*3*/
-          if (i >= _athletesList.length) {
-            _athletesList.addAll(snapshot.data.take(100));
-          }
-
-          return _buildRow(_athletesList[i]);
-        },
-      );
+        return _buildRow(_athletesList[i]);
+      },
+    );
   }
 
   Widget _buildRow(Athlete a) {
     final alreadyBought = _boughtAthletes.contains(a);
-
 
     return Card(
       color: Colors.blueAccent,
@@ -107,13 +78,15 @@ class _AthletesListState extends State<AthletesList> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Buy an Athlete"),
-        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+        actions: [
+          IconButton(icon: Icon(Icons.account_balance_rounded), onPressed: () {})
+        ],
       ),
       body: RefreshIndicator(
           onRefresh: _loadData,
           child: Center(
             child: FutureBuilder<List<Athlete>>(
-              future: _loadData(),
+              future: fetchAthletes(),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   return _buildAthletes(snapshot);
