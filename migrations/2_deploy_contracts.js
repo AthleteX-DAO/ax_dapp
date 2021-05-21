ff7f95a5476b2465f63b9becf27ef14dbd18e450
Kevin Kamto <kamtokevin@gmail.com>
2021-05-21 15:41:01 -0400

    clean: cleanup of unnecessary files

--- migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const HelloWorld = artifacts.require("HelloWorld")
-const AECoin = artifacts.require("AECoin");
-module.exports = function (deployer) {
-    deployer.deploy(AECoin);
-    deployer.deploy(HelloWorld);
-}
