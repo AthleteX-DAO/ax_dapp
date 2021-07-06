260a0450130f2b02939fa0917d74e06767b2b884
Kevin Kamto <kamtokevin@gmail.com>
2021-07-06 19:35:22 -0400

    feat: wiping swap.dart (plan to rewrite)

--- lib/pages/NavigationBar.dart
+++ lib/pages/NavigationBar.dart
@@ -3,7 +3,6 @@ import "package:velocity_x/velocity_x.dart";
 import 'package:ae_dapp/pages/wallet.dart';
 import 'package:ae_dapp/pages/AthletesList.dart';
 import 'package:ae_dapp/pages/MyTeam.dart';
-import 'package:ae_dapp/pages/swap.dart';
 
 class NavigationBar extends StatefulWidget {
   NavigationBar({Key? key}) : super(key: key);
@@ -18,7 +17,7 @@ class _NavigationBarState extends State<NavigationBar> {
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   // This is where the pages are linked to the navigation
-  List<Widget> _widgetOptions = <Widget>[Wallet(), MyTeam(), Swap()];
+  List<Widget> _widgetOptions = <Widget>[Wallet(), MyTeam(), Text("This deserves a time out! We need to review the play and update this", style: TextStyle(fontSize: 21.2, color: Colors.amber[400],)),];
 
   void _onItemTapped(int index) {
     setState(() {
