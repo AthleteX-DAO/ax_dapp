7f8a1a51a8366ab8822b42e2c00df03418df266d
Kevin Kamto <kamtokevin@gmail.com>
2021-07-06 14:54:22 -0400

    feat: updated myTeams page for bought athletes

--- lib/pages/MyTeam.dart
+++ lib/pages/MyTeam.dart
@@ -1,6 +1,6 @@
+import 'package:ae_dapp/pages/AthletesList.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_sparkline/flutter_sparkline.dart';
-
+import 'package:draggable_home/draggable_home.dart';
 
 
 class MyTeam extends StatefulWidget {
@@ -11,45 +11,77 @@ class MyTeam extends StatefulWidget {
 }
 
 class _MyTeamState extends State<MyTeam> {
-
   var data = [0.0, 1.0, 1.5, 2.0, 0.0, 0.0, -0.5, -1.0, -0.5, 0.0, 0.0];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My Team"),
-      ),
-      body: new Center(
-        child: Column(
-          children: <Widget>[
-            new Expanded(
-              flex: 1,
-              child: Text("Your portfolio history"),
+    Container headerBottomBarWidget() {
+      return Container(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.arrow_downward,
+              color: Colors.white,
             ),
-            new Expanded(
-              flex: 3,
-              child: Container(
-                child: new Sparkline(
-                  data: data,
-                  lineWidth: 10.0,
-                  fillMode: FillMode.below,
-                  fillColor: Colors.amber[700],
-                  pointsMode: PointsMode.all,
-                  pointColor: Colors.red[400],
-                ),
-              ),
-            ),
-            new Expanded(
-              flex: 3,
-              child: Container(
-                // Listview goes here
-                child: new Text("List of your Athletes goes here!"),
-              ),
-            ),
+            Text("Swipe down to buy athletes")
           ],
         ),
-      ),
+      );
+    }
+
+    Container headerWidget(BuildContext context) => Container(
+          child: Center(
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+            new Text("Portfolio history goes here")
+            //       new Sparkline(data: data,
+            // lineWidth: 10.0,
+            // fillMode: FillMode.below,
+            // fillColor: Colors.amber[700],
+            // pointsMode: PointsMode.all,
+            // pointSize: 3.0,
+            // pointColor: Colors.black)
+                ],
+              ),
+            ),
+          ),
+        );
+
+    ListView listView() {
+      return ListView.builder(
+        padding: EdgeInsets.only(top: 0),
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: 2,
+        shrinkWrap: true,
+        itemBuilder: (context, index) => Card(
+          color: Colors.white70,
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Text("$index"),
+            ),
+            title: Text("Athlete"),
+            subtitle: Text("Player"),
+          ),
+        ),
+      );
+    }
+
+    return DraggableHome(
+      leading: Icon(Icons.arrow_back),
+      title: Text("My Team"),
+      headerWidget: headerWidget(context),
+      headerBottomBar: headerBottomBarWidget(),
+      body: [
+        Row(children: [Text("Your Athletes", style: TextStyle(color: Colors.amber[400], fontWeight: FontWeight.bold),)], mainAxisAlignment: MainAxisAlignment.center,),
+        listView()],
+      fullyStretchable: true,
+      expandedBody: AthletesList(),
     );
   }
 }
