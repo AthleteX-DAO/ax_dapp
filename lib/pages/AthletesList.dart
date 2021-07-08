51cf85c31b858446235ad646356916d13e2f8da1
Kevin Kamto <kamtokevin@gmail.com>
2021-07-08 12:46:08 -0400

    fix(WIP): fixing search functionality

--- lib/pages/AthletesList.dart
+++ lib/pages/AthletesList.dart
@@ -16,95 +16,32 @@ class _AllAthletesListState extends State<AthletesList> {
   List filteredNames = <Athlete>[]; // names filtered by search text
   Icon _searchIcon = new Icon(Icons.search);
   final _boughtAthletes = <Athlete>{};
-  Future<dynamic>? _loadData;
+  List<Athlete>? __athletesDataEntity;
   String _searchText = "";
 
   @override
   void initState() {
     // TODO: implement initState
     //
-    _filter.addListener(() {
-      if (_filter.text.isEmpty) {
-        setState(() {
-          _searchText = "";
-          updateFilter(_searchText);
-        });
-      } else {
-        setState(() {
-          _searchText = _filter.text;
-          updateFilter(_searchText);
-        });
-      }
-    });
-    _loadData = _loadAthletes();
+
+    initList(); //sets up synchronous array of athletes from async pull
     super.initState();
   }
 
-  void updateFilter(String text) {
-    print("updated Text: $text");
+  initList() async {
+    __athletesDataEntity = await _loadAthletes();
   }
 
-  Future<dynamic> _loadAthletes() async {
+  Future<List<Athlete>> _loadAthletes() async {
     return await fetchAthletes();
   }
-
-/*
-  void _searchPressed(String title) {
-    setState(() {
-      if (this._searchIcon.icon == Icons.search) {
-        this._searchIcon = new Icon(Icons.close);
-        this._appBarTitle = new TextField(
-          style: setTextStyle(),
-          controller: _filter,
-          decoration: new InputDecoration(
-              // Probably need to abstract this out later - vx for search
-              focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black)),
-              prefixIcon: new Icon(
-                Icons.search,
-                color: Colors.black,
-              ),
-              hintText: 'Search...',
-              hintStyle: setTextStyle()),
-        );
-      } else {
-        this._searchIcon = new Icon(Icons.search);
-        this._appBarTitle = new Text(title);
-        _filter.clear();
-        this._appBarTitle = null;
-      }
-    });
-  }
-  */
 
   setTextStyle() {
     return TextStyle(color: Colors.blueGrey);
   }
 
-  searchFilter(int i) {
-    //     if (_athletesSearchableList[i]
-    //     .name
-    //     .toLowerCase()
-    //     .contains(_searchText.toLowerCase())) {
-    //   _athleteFilteredList.add(_athletesSearchableList[i]);
-    // }
-  }
-
-  void _AthleteDetails(Athlete aAthlete) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(builder: (BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("About ${aAthlete.name}"),
-          ),
-          body: AthleteDetail(),
-        );
-      }),
-    );
-  }
-
-  Widget _buildAthletes(AsyncSnapshot<dynamic> snapshot) {
-    _AllAthletesList.addAll(snapshot.data!);
+  Widget _buildAthletesList(List<Athlete>? _futureAthletes) {
+    _AllAthletesList.addAll(_futureAthletes as List<Athlete>);
 
     return ListView.builder(
       itemCount: _AllAthletesList.length,
@@ -119,7 +56,6 @@ class _AllAthletesListState extends State<AthletesList> {
 
   Widget _buildRow(Athlete a) {
     final alreadyBought = _boughtAthletes.contains(a);
-
     return Card(
       color: Colors.blueAccent,
       child: Column(
@@ -139,7 +75,6 @@ class _AllAthletesListState extends State<AthletesList> {
                 : Icon(Icons.check_circle_outline),
             onTap: () {
               setState(() {
-                _AthleteDetails(a);
                 if (alreadyBought) {
                   _boughtAthletes.remove(a);
                 } else {
@@ -164,31 +99,11 @@ class _AllAthletesListState extends State<AthletesList> {
           IconButton(
               icon: _searchIcon,
               onPressed: () {
-  //              _searchPressed("Search an Athlete");
+                //              _searchPressed("Search an Athlete");
               })
         ],
       ),
-      body: FutureBuilder<dynamic>(
-              future: _loadData,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return RefreshIndicator(
-                    onRefresh: () {
-                      return _loadData = fetchAthletes();
-                    },
-                    child: Center(
-                      child: _buildAthletes(snapshot),
-                    )
-                  );
-                } else if (snapshot.hasError) {
-                  return Text(
-                      "Something went wrong! make sure you're connected to the internet");
-                }
-                return CircularProgressIndicator(
-                  backgroundColor: Colors.yellowAccent,
-                );
-              },
-            ),
+      body: _buildAthletesList(__athletesDataEntity),
     );
   }
 }
