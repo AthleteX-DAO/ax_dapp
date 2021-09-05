99bd9b1a8cd572f6e6ea9d0820e42d2435cc3260
Kevin Kamto <kamtokevin@gmail.com>
2021-09-05 16:51:58 -0400

    refactor: stripped controller

--- lib/service/Controller.dart
+++ lib/service/Controller.dart
@@ -1,18 +1,16 @@
 import 'package:flutter_web3/ethereum.dart';
-import 'package:flutter_web3/flutter_web3.dart';
-import 'package:web3dart/browser.dart';
 import 'package:web3dart/web3dart.dart';
 import 'dart:html';
 
 class Controller {
   // RPC & WS are now linked to MATIC-Testnet
 
-  late JsonRpcProvider rpcProvider;
   final String _rpcUrl = 'https://rpc-mumbai.matic.today';
   final String _wsUrl = 'wss://rpc-mumbai.matic.today';
   var eth;
   var _client;
   var _credentials;
+  bool ethIsEnabled = false;
 
   // final Contract staking = Contract(
   //     "0x063086C5b352F986718Db9383c894Be9Cd4350fA", abi, provider!.getSigner());
@@ -20,19 +18,9 @@ class Controller {
   //     ContractERC20("0x585E0c93F73C520ca6513fc03f450dAea3D4b493", ethereum!);
 
   // No-args constructor
-  Controller() {
-    if (eth == null) {
-      print('MetaMask is not available');
-      return;
-    }
-
-    init();
-  }
-
-  init() async {
-    eth = window.ethereum;
-    _client = Web3Client.custom(eth!.asRpcService());
-    _credentials = await eth.requestAccount();
+  Controller() 
+  {
+    
   }
 
   // Getters
