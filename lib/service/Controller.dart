4f602e76c9d79586dd78f640f8d98cd69455862a
Kevin Kamto <kamtokevin@gmail.com>
2021-09-05 20:56:04 -0400

    refactor(Controller): simplified controller

--- lib/service/Controller.dart
+++ lib/service/Controller.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_web3/ethereum.dart';
+
 import 'package:web3dart/web3dart.dart';
 import 'dart:html';
 
@@ -7,23 +7,35 @@ class Controller {
 
   final String _rpcUrl = 'https://rpc-mumbai.matic.today';
   final String _wsUrl = 'wss://rpc-mumbai.matic.today';
+  // Placeholder for ethers library (window.ethereum)
+  static const ACTIVE_CHAIN_ID = 80001;
   var eth;
-  var _client;
-  var _credentials;
+  Web3Client? _client;
+  Credentials? _credentials;
   bool ethIsEnabled = false;
+  bool activeChain = false;
 
   // final Contract staking = Contract(
   //     "0x063086C5b352F986718Db9383c894Be9Cd4350fA", abi, provider!.getSigner());
   // final ContractERC20 _axToken =
   //     ContractERC20("0x585E0c93F73C520ca6513fc03f450dAea3D4b493", ethereum!);
-
-  // No-args constructor
-  Controller() 
-  {
-    
+  Controller() {
+    // TODO - init controller variables
   }
 
   // Getters
-  Web3Client get client => _client;
-  Credentials get credentials => _credentials;
+  Web3Client? get client => _client;
+  Credentials? get credentials => _credentials;
+
+  void updateClient(Web3Client _newClient) {
+    _client = _newClient;
+  }
+
+  bool checkCorrectChain() {
+
+    // ignore: unrelated_type_equality_checks
+    _client!.getNetworkId() == ACTIVE_CHAIN_ID ? activeChain = true : activeChain = false;
+
+    return activeChain;
+  }
 }
