7c3e07ce63f432ea413345f2f890b25f100d914b
rmilrad <ryan.milrad@yahoo.com>
2021-11-06 14:45:43 -0400

    code changes for errors'

--- lib/pages/KeyGenerationPage.dart
+++ lib/pages/KeyGenerationPage.dart
@@ -1,5 +1,5 @@
+import 'package:ae_dapp/pages/HomePage.dart';
 import 'package:flutter/material.dart';
-import 'package:ae_dapp/pages/NavigationBar.dart';
 import 'package:ae_dapp/service/Singleton.dart';
 
 class KeyGenerationPage extends StatefulWidget {
@@ -50,24 +50,22 @@ class _KeyGenerationPageState extends State<KeyGenerationPage> {
           Column(
             children: <Widget>[
               Align(
-                alignment: Alignment.topRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: Image(
-                    width: 80,
-                    height: 80,
-                    image: AssetImage("assets/images/x.png"),
-                  ),
-                )
-              ),
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: Image(
+                      width: 80,
+                      height: 80,
+                      image: AssetImage("assets/images/x.png"),
+                    ),
+                  )),
               Text("Your New Key",
-                style: TextStyle(
-                  fontFamily: 'OpenSans',
-                  fontSize: lgTxSize,
-                  fontWeight: FontWeight.w600,
-                  fontStyle: FontStyle.italic,
-                )
-              ),
+                  style: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: lgTxSize,
+                    fontWeight: FontWeight.w600,
+                    fontStyle: FontStyle.italic,
+                  )),
               child,
             ],
           ),
@@ -81,97 +79,80 @@ class _KeyGenerationPageState extends State<KeyGenerationPage> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-          padding: EdgeInsets.only(top: 80, bottom: 20),
-          child: Text("Your new Mnemonic is:",
-            style: TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: 18,
-            )
-          )
-        ),
+            padding: EdgeInsets.only(top: 80, bottom: 20),
+            child: Text("Your new Mnemonic is:",
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 18,
+                ))),
         Container(
-          padding: EdgeInsets.symmetric(vertical: 20),
-          width: MediaQuery.of(context).size.width * .75,
-          height: MediaQuery.of(context).size.height * .1,
-          decoration: BoxDecoration(
-            color: Colors.grey[900],
-            borderRadius: BorderRadius.circular(10)
-          ),
-          alignment: Alignment.center,
-          child: SelectableText(_mnemonic,
-            style: TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: 24,
-              fontWeight: FontWeight.w600,
-              color: Colors.amber[400],
-            )
-          )
-        ),
+            padding: EdgeInsets.symmetric(vertical: 20),
+            width: MediaQuery.of(context).size.width * .75,
+            height: MediaQuery.of(context).size.height * .1,
+            decoration: BoxDecoration(
+                color: Colors.grey[900],
+                borderRadius: BorderRadius.circular(10)),
+            alignment: Alignment.center,
+            child: SelectableText(_mnemonic,
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.amber[400],
+                ))),
         Container(
-          padding: EdgeInsets.only(top: 20, bottom: 50),
-          child: Text("Keep this key and save it for your future use",
-            style: TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: 18,
-            )
-          )
-        ),
+            padding: EdgeInsets.only(top: 20, bottom: 50),
+            child: Text("Keep this key and save it for your future use",
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 18,
+                ))),
         Container(
-          padding: EdgeInsets.only(top: 20, bottom: 50),
-          child: Text("DO NOT LOSE THIS",
-            style: TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: 18,
-            )
-          )
-        ),
+            padding: EdgeInsets.only(top: 20, bottom: 50),
+            child: Text("DO NOT LOSE THIS",
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 18,
+                ))),
         ConstrainedBox(
-          constraints: BoxConstraints.tightFor(width: 250, height: 55),
-          child: ElevatedButton(
-            child: Text("START TRADING",
-              style: TextStyle(
-                fontFamily: 'OpenSans',
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-                color: Colors.amber[400],
-              )
-            ),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => NavigationBar()),
-              );
-            },
-            style: ButtonStyle(
-              backgroundColor:
-                MaterialStateProperty.all<Color>(Colors.transparent),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.amber[400]!),
-                )
-              )
-            ),
-          )
-        ),
+            constraints: BoxConstraints.tightFor(width: 250, height: 55),
+            child: ElevatedButton(
+              child: Text("START TRADING",
+                  style: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.amber[400],
+                  )),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => HomePage()),
+                );
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.transparent),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.amber[400]!),
+                  ))),
+            )),
       ],
     );
   }
 
   Widget notReadyChild() {
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          child: CircularProgressIndicator(),
-          width: 60,
-          height: 60,
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 16),
-          child: Text('Awaiting Mnemonic Key...'),
-        )
-      ]
-    );
+    return Column(children: <Widget>[
+      SizedBox(
+        child: CircularProgressIndicator(),
+        width: 60,
+        height: 60,
+      ),
+      Padding(
+        padding: EdgeInsets.only(top: 16),
+        child: Text('Awaiting Mnemonic Key...'),
+      )
+    ]);
   }
-
 }
-
