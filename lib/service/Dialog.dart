ba2b248a99dc68d51088e666c01b5cfb83564cac
Ryan Bharath <dbossman60@gmail.com>
2021-12-13 14:09:15 -0500

    Update Dialog.dart

--- lib/service/Dialog.dart
+++ lib/service/Dialog.dart
@@ -191,6 +191,9 @@ Dialog buyDialog(BuildContext context, Athlete athlete) {
     ),
     child: Container(
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
       height: MediaQuery.of(context).size.height * 0.57,
       width: MediaQuery.of(context).size.width * (1 / 5),
       decoration: boxDecoration(Colors.grey[900]!, 30, 0, Colors.black),
@@ -762,6 +765,9 @@ Dialog sellDialog(BuildContext context, Athlete athlete) {
                       Container(
                         child: Text(
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
                           athlete.name + " APT",
                           style: textStyle(Colors.white, 15, false, false),
                         ),
@@ -834,6 +840,7 @@ Dialog sellDialog(BuildContext context, Athlete athlete) {
                           ),
                         ),
                       ),
+<<<<<<< HEAD
                       Container(
                         child: Text(
                           "L.J.APT",
@@ -841,6 +848,15 @@ Dialog sellDialog(BuildContext context, Athlete athlete) {
                             fontSize: 15,
                             color: Colors.grey[600],
                           ),
+=======
+                    ),
+                    Container(
+                      child: Text(
+                        "0.8 " + athlete.name + " APT per AX",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
                         ),
                       ),
                     ],
@@ -949,6 +965,7 @@ Dialog sellDialog(BuildContext context, Athlete athlete) {
   double hgt = 0.6;
 
   return Dialog(
+<<<<<<< HEAD
     backgroundColor: Colors.transparent,
     shape: RoundedRectangleBorder(  
       borderRadius: BorderRadius.circular(12.0),
@@ -1053,10 +1070,49 @@ Dialog sellDialog(BuildContext context, Athlete athlete) {
                             inputFormatters: [
                               FilteringTextInputFormatter.allow((RegExp(r'^(\d+)?\.?\d{0,2}'))),
                             ],
+=======
+      backgroundColor: Colors.transparent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Container(
+          height: MediaQuery.of(context).size.height * 0.45,
+          width: MediaQuery.of(context).size.width * (2 / 9),
+          decoration: boxDecoration(Colors.grey[900]!, 30, 0, Colors.black),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                height: MediaQuery.of(context).size.height * .25,
+                width: 350,
+                //color: Colors.red,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(top: 15.0),
+                          child: Text(
+                            "Redeem " + athlete.name + " APT",
+                            style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 15.0),
+                          child: IconButton(
+                            icon: const Icon(Icons.close, color: Colors.white),
+                            onPressed: () => Navigator.pop(context),
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
                           ),
                         ),
                       ],
                     ),
+<<<<<<< HEAD
                   ),
                 ],
               )
@@ -1108,9 +1164,66 @@ Dialog sellDialog(BuildContext context, Athlete athlete) {
                           style: TextStyle(
                             fontSize: 15,
                             color: Colors.grey[600],
+=======
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          child: Expanded(
+                            child: RichText(
+                              text: TextSpan(
+                                children: <TextSpan>[
+                                  TextSpan(
+                                      text:
+                                          "You can redeem APT's at their Book Value for AX.",
+                                      style: TextStyle(
+                                          color: Colors.grey[600],
+                                          fontSize: 15)),
+                                  TextSpan(
+                                      text:
+                                          " You can access other funds with AX on the Matic network through",
+                                      style: TextStyle(
+                                          color: Colors.grey[600],
+                                          fontSize: 15)),
+                                  TextSpan(
+                                      text: " SushiSwap",
+                                      style: TextStyle(
+                                          color: Colors.amber[400],
+                                          fontSize: 15)),
+                                ],
+                              ),
+                            ),
                           ),
                         ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(top: 40.0),
+                          child: Text(
+                            "Input AX:",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey[600],
+                            ),
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      width: 400,
+                      height: 55,
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(14.0),
+                        border: Border.all(
+                          color: Colors.grey[400]!,
+                          width: 0.5,
+                        ),
                       ),
+<<<<<<< HEAD
                     ],
                   ),
                   Row(
@@ -1437,6 +1550,94 @@ Dialog redeemDialog(BuildContext context, Athlete athlete) {
                       ],
                     ),
 <<<<<<< HEAD
+=======
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(
+                              athlete.name + " APT",
+                              style: textStyle(Colors.white, 15, false, false),
+                            ),
+                          ),
+                          Container(
+                            height: 18,
+                            width: 35,
+                            decoration: boxDecoration(Colors.transparent, 100,
+                                0.5, Colors.grey[400]!),
+                            child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Max",
+                                style: textStyle(
+                                    Colors.grey[400]!, 9, false, false),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 70,
+                            child: TextField(
+                              style: textStyle(
+                                  Colors.grey[400]!, 22, false, false),
+                              decoration: InputDecoration(
+                                hintText: '0.00',
+                                hintStyle: textStyle(
+                                    Colors.grey[400]!, 22, false, false),
+                                contentPadding: const EdgeInsets.all(9),
+                                border: InputBorder.none,
+                              ),
+                              inputFormatters: [
+                                FilteringTextInputFormatter.allow(
+                                    (RegExp(r'^(\d+)?\.?\d{0,2}'))),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Divider(
+                  thickness: 0.35,
+                  color: Colors.grey[400],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10.0),
+                height: MediaQuery.of(context).size.height * .15,
+                width: 350,
+                //color: Colors.red,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(bottom: 5.0),
+                          child: Text(
+                            "You recieve:",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(bottom: 5.0),
+                          child: Text(
+                            "120 AX",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
@@ -1457,6 +1658,7 @@ Dialog redeemDialog(BuildContext context, Athlete athlete) {
                                 color: Colors.black,
                               ),
                             ),
+<<<<<<< HEAD
 =======
                   ],
                 ),
@@ -1482,6 +1684,8 @@ Dialog redeemDialog(BuildContext context, Athlete athlete) {
                             fontSize: 16,
                             color: Colors.black,
 >>>>>>> 1584999326735e8fecdc6edb20ef279089b8c083
+=======
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
                           ),
                         ),
                       ],
@@ -1679,6 +1883,9 @@ Dialog mintDialog(BuildContext context, Athlete athlete) {
                       ],
                     ),
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
@@ -1699,6 +1906,7 @@ Dialog mintDialog(BuildContext context, Athlete athlete) {
                                 color: Colors.black,
                               ),
                             ),
+<<<<<<< HEAD
 =======
                   ],
                 ),
@@ -1724,6 +1932,8 @@ Dialog mintDialog(BuildContext context, Athlete athlete) {
                             fontSize: 16,
                             color: Colors.black,
 >>>>>>> 1584999326735e8fecdc6edb20ef279089b8c083
+=======
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
                           ),
                         ),
                       ],
@@ -1733,6 +1943,7 @@ Dialog mintDialog(BuildContext context, Athlete athlete) {
               ),
             ],
           )));
+<<<<<<< HEAD
 }
 
 <<<<<<< HEAD
@@ -1897,6 +2108,42 @@ Dialog confirmTransaction(BuildContext context) {
   BoxDecoration boxDecoration(Color col, double rad, double borWid, Color borCol) {
     return BoxDecoration(
 >>>>>>> 1584999326735e8fecdc6edb20ef279089b8c083
+=======
+}
+
+TextStyle textStyle(Color color, double size, bool isBold, bool isUline) {
+  if (isBold) if (isUline)
+    return TextStyle(
+        color: color,
+        fontFamily: 'OpenSans',
+        fontSize: size,
+        fontWeight: FontWeight.w400,
+        decoration: TextDecoration.underline);
+  else
+    return TextStyle(
+      color: color,
+      fontFamily: 'OpenSans',
+      fontSize: size,
+      fontWeight: FontWeight.w400,
+    );
+  else if (isUline)
+    return TextStyle(
+        color: color,
+        fontFamily: 'OpenSans',
+        fontSize: size,
+        decoration: TextDecoration.underline);
+  else
+    return TextStyle(
+      color: color,
+      fontFamily: 'OpenSans',
+      fontSize: size,
+    );
+}
+
+BoxDecoration boxDecoration(
+    Color col, double rad, double borWid, Color borCol) {
+  return BoxDecoration(
+>>>>>>> parent of 4ae636c (Revert "feat: added deposit pop-up UI for Farm section")
       color: col,
       borderRadius: BorderRadius.circular(rad),
       border: Border.all(color: borCol, width: borWid));
