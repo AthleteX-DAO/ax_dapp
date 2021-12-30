9005268a1893b8a01e0ce29910631955f15c15d4
Kevin Kamto <kamtokevin@gmail.com>
2021-12-30 10:25:57 -0500

    test: axt verify address

--- lib/service/Controller/AXT.dart
+++ lib/service/Controller/AXT.dart
@@ -5,9 +5,9 @@ import 'Token.dart';
 class AXT extends Token {
   String axName = "";
 
-  static EthereumAddress polygonAddress =
+  final EthereumAddress polygonAddress =
       EthereumAddress.fromHex("0x5617604ba0a30e0ff1d2163ab94e50d8b6d0b0df");
-  static EthereumAddress mumbaiAddress =
+  final EthereumAddress mumbaiAddress =
       EthereumAddress.fromHex("0x76d9a6e4cdefc840a47069b71824ad8ff4819e85");
 
   AXT(String name, String ticker, [AssetImage? icon]) : super(name, ticker) {
---
+++ test/AXT_test.dart
@@ -0,0 +1,9 @@
+import 'package:ax_dapp/service/Controller/AXT.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('verify mainnet address', () {
+    final axt = AXT("AthleteX", "AX");
+    expect(axt.polygonAddress, '0x5617604ba0a30e0ff1d2163ab94e50d8b6d0b0df');
+  });
+}
