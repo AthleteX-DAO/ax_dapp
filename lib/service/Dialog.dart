33716e6da9ded8678188d7fa95b7f3bd2d4e2562
Ryan Bharath <dbossman60@gmail.com>
2022-01-02 00:46:18 -0500

    chore: remove liquidity popup UI under my farms page is dynamic

--- lib/service/Dialog.dart
+++ lib/service/Dialog.dart
@@ -1433,8 +1433,7 @@ Dialog mintDialog(BuildContext context, Athlete athlete) {
 }
 
 // dynamic
-Dialog confirmTransaction(
-    BuildContext context, bool IsConfirmed, String txString) {
+Dialog confirmTransaction(BuildContext context, bool IsConfirmed, String txString) {
   double _height = MediaQuery.of(context).size.height;
   double _width = MediaQuery.of(context).size.width;
   double wid = 500;
@@ -2007,6 +2006,13 @@ Dialog accountDialog(BuildContext context) {
 
 Dialog removeDialog(BuildContext context) {
   double amount = 0;
+  double _height = MediaQuery.of(context).size.height;
+  double _width = MediaQuery.of(context).size.width;
+  double wid = 390;
+  double edge = 60;
+  if (_width < 395) wid = _width;
+  double hgt = 450;
+  if (_height < 455) hgt = _height;
 
   return Dialog(
     backgroundColor: Colors.transparent,
@@ -2014,8 +2020,8 @@ Dialog removeDialog(BuildContext context) {
       borderRadius: BorderRadius.circular(12.0),
     ),
     child: Container(
-      height: MediaQuery.of(context).size.height * 0.55,
-      width: MediaQuery.of(context).size.width * 0.25,
+      height: hgt,
+      width: wid,
       padding: EdgeInsets.symmetric(vertical: 22, horizontal: 30),
       decoration: boxDecoration(Colors.grey[900]!, 30, 0, Colors.black),
       child: Column(
@@ -2048,7 +2054,7 @@ Dialog removeDialog(BuildContext context) {
               Container(
                 margin: const EdgeInsets.symmetric(vertical: 30),
                 padding: const EdgeInsets.all(10),
-                width: MediaQuery.of(context).size.width * 0.2,
+                width: wid - edge,
                 height: 55,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
