df53463fe7a9f57f174d3ab25959b72588290093
Kevin Kamto <kamtokevin@gmail.com>
2022-01-11 18:12:05 -0500

    refactor: improvement of how to query prices

--- lib/service/Controller/Scout/Prices.dart
+++ lib/service/Controller/Scout/Prices.dart
@@ -1,10 +1,11 @@
-// File for getting the price of any APT  from the DEX
+// File for getting the price of any APT  from the DEX using Postgres instead of http
 
 import 'package:ax_dapp/service/Athlete.dart';
+import 'package:postgres/postgres.dart';
 
 class Prices {
   late Athlete athlete;
-
+  late PostgreSQLConnection connection;
   var databaseUrl;
   var firstPrice, lastPrice;
 
@@ -12,14 +13,47 @@ class Prices {
     //
   }
 
-  void getMarketPrice() {}
-
-  void query(String name) {
-    String query =
-        "SELECT $name, first(price) as StartingPrice, last(price) as LatestPrice from nfl;";
+  Prices() {
+    confirmConnection();
   }
 
-  void getLatestAXPrice() {
-    String query = "";
+  void connect() async {
+    connection = PostgreSQLConnection("139.99.74.201", 8812, "qdb",
+        username: "admin", password: "quest");
+  }
+
+  void confirmConnection() async {
+    connection = PostgreSQLConnection("139.99.74.201", 8812, "qdb",
+        username: "admin", password: "quest");
+    await connection
+        .open()
+        .then((value) => {print('Connection confirmed /n $value')});
+  }
+
+  void getMarketPrice() {}
+
+  void query(String name) async {
+    print("QUERY: this came in: $name");
+    String query =
+        "SELECT $name, first(price) as StartingPrice, last(price) as LatestPrice from nfl;";
+
+    List<List<dynamic>> results = await connection.query(
+        "SELECT @name, first(price) as StartingPrice, last(price) as LatestPrice from nfl",
+        substitutionValues: {"name": name});
+
+    for (final row in results) {
+      var a = row[0];
+      var b = row[0];
+      print("results: $a \n $b");
+    }
+
+    // List<List<dynamic>> results = await connection.query("SELECT a, b FROM table WHERE a = @aValue", substitutionValues: {
+    //     "aValue" : 3
+    // });
+  }
+
+  void close() async {
+    // close the Postgresql connection
+    connection.close();
   }
 }
--- test/Prices_test.dart
+++ test/Prices_test.dart
@@ -1,10 +1,12 @@
 @TestOn('browser')
-import 'package:ax_dapp/service/Controller/Swap/AXT.dart';
+import 'package:ax_dapp/service/Controller/Scout/Prices.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('verify mainnet address', () {
-    final axt = AXT("AthleteX", "AX");
-    expect(AXT.polygonAddress, '0x5617604ba0a30e0ff1d2163ab94e50d8b6d0b0df');
+    final Prices prices = Prices();
+    prices.query("T.Brady");
+    // Submit a query
+    expect("0x5617604ba0a30e0ff1d2163ab94e50d8b6d0b0df", '0x5617604ba0a30e0ff1d2163ab94e50d8b6d0b0df');
   });
 }
