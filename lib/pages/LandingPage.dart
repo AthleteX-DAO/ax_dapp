f165af05187ec34c02b3cd6786d8b171539e082b
mretana1999 <mretana1999@gmail.com>
2022-01-15 18:47:09 -0500

    chore: changed size of start trading button on the landing page

--- lib/pages/LandingPage.dart
+++ lib/pages/LandingPage.dart
@@ -14,16 +14,14 @@ class _LandingPageState extends State<LandingPage> {
   bool next = false;
   @override
   Widget build(BuildContext context) {
+    if (next) return V1App();
 
-    if (next)
-      return V1App();
-      
     double _width = MediaQuery.of(context).size.width;
     double _height = MediaQuery.of(context).size.height;
-    double textSize = _height*0.05;
-    if (_width < _height)
-      textSize = _width*0.05;
-      
+    double textSize = _height * 0.05;
+    double tradingTextSize = textSize * 0.7;
+    if (_width < _height) textSize = _width * 0.05;
+
     return Container(
       width: _width,
       height: _height,
@@ -33,19 +31,18 @@ class _LandingPageState extends State<LandingPage> {
           fit: BoxFit.fill,
         ),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          //AX Markets Image
-          Container(
-            height: _height*0.2,
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <
+              Widget>[
+        //AX Markets Image
+        Container(
+            height: _height * 0.2,
             child: Image(
               image: AssetImage("../assets/images/AXMarkets.png"),
-            )
-          ),
-          // Text
-          Container(
-            height: _height*0.3,
+            )),
+        // Text
+        Container(
+            height: _height * 0.3,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -53,35 +50,53 @@ class _LandingPageState extends State<LandingPage> {
                 RichText(
                   text: TextSpan(
                     children: <TextSpan>[
-                      TextSpan(text: "TRADE", style: textStyle(Colors.amber[400]!, textSize, true, false)),
-                      TextSpan(text: " ATHLETES", style: textStyle(Colors.white, textSize, true, false)),
+                      TextSpan(
+                          text: "TRADE",
+                          style: textStyle(
+                              Colors.amber[400]!, textSize, true, false)),
+                      TextSpan(
+                          text: " ATHLETES",
+                          style:
+                              textStyle(Colors.white, textSize, true, false)),
                     ],
                   ),
                 ),
                 RichText(
                   text: TextSpan(
                     children: <TextSpan>[
-                      TextSpan(text: "BUILD", style: textStyle(Colors.amber[400]!, textSize, true, false)),
-                      TextSpan(text: " YOUR ROSTER", style: textStyle(Colors.white, textSize, true, false)),
+                      TextSpan(
+                          text: "BUILD",
+                          style: textStyle(
+                              Colors.amber[400]!, textSize, true, false)),
+                      TextSpan(
+                          text: " YOUR ROSTER",
+                          style:
+                              textStyle(Colors.white, textSize, true, false)),
                     ],
                   ),
                 ),
                 RichText(
                   text: TextSpan(
                     children: <TextSpan>[
-                      TextSpan(text: "EARN", style: textStyle(Colors.amber[400]!, textSize, true, false)),
-                      TextSpan(text: " REWARDS", style: textStyle(Colors.white, textSize, true, false)),
+                      TextSpan(
+                          text: "EARN",
+                          style: textStyle(
+                              Colors.amber[400]!, textSize, true, false)),
+                      TextSpan(
+                          text: " REWARDS",
+                          style:
+                              textStyle(Colors.white, textSize, true, false)),
                     ],
                   ),
                 ),
               ],
-            )
-          ),
-          //Button load athletes
-          Container(
-            width: _width*0.25,
-            height: _height*0.1,
-            child: FutureBuilder<dynamic>(
+            )),
+        //Button load athletes
+        Container(
+          width: _width * 0.20,
+          height: _height * 0.08,
+          margin: EdgeInsets.only(bottom: 130.0),
+          child: FutureBuilder<dynamic>(
               future: AthleteApi.getAthletesLocally(context),
               builder: (context, snapshot) {
                 switch (snapshot.connectionState) {
@@ -90,67 +105,64 @@ class _LandingPageState extends State<LandingPage> {
                       child: CircularProgressIndicator(),
                     );
                   default:
-                  AthleteList.list = snapshot.data;
+                    AthleteList.list = snapshot.data;
                     return Container(
-                      decoration: boxDecoration(Colors.transparent, 100, 4, Colors.amber[400]!),
+                      decoration: boxDecoration(
+                          Colors.transparent, 100, 4, Colors.amber[400]!),
                       child: TextButton(
-                        onPressed: () {setState(() {next = true;});},
+                        onPressed: () {
+                          setState(() {
+                            next = true;
+                          });
+                        },
                         child: Text(
                           "Start Trading",
-                          style: textStyle(Colors.amber[400]!, textSize, true, false),
-                        )
-                      )
+                          style: textStyle(
+                              Colors.amber[400]!, tradingTextSize, true, false),
+                        ),
+                      ),
                     );
                 }
-              }
-            )
-          ),
-        ]
-      )
+              }),
+        ),
+      ]),
     );
   }
 
   TextStyle textStyle(Color color, double size, bool isBold, bool isUline) {
-    if (isBold)
-      if (isUline)
-        return TextStyle(
+    if (isBold) if (isUline)
+      return TextStyle(
           color: color,
           fontFamily: 'OpenSans',
           fontSize: size,
           fontWeight: FontWeight.w400,
-          decoration: TextDecoration.underline
-        );
-      else
-        return TextStyle(
-          color: color,
-          fontFamily: 'OpenSans',
-          fontSize: size,
-          fontWeight: FontWeight.w400,
-        );
+          decoration: TextDecoration.underline);
     else
-      if (isUline)
-        return TextStyle(
+      return TextStyle(
+        color: color,
+        fontFamily: 'OpenSans',
+        fontSize: size,
+        fontWeight: FontWeight.w400,
+      );
+    else if (isUline)
+      return TextStyle(
           color: color,
           fontFamily: 'OpenSans',
           fontSize: size,
-          decoration: TextDecoration.underline
-        );
-      else
-        return TextStyle(
-          color: color,
-          fontFamily: 'OpenSans',
-          fontSize: size,
-        );
+          decoration: TextDecoration.underline);
+    else
+      return TextStyle(
+        color: color,
+        fontFamily: 'OpenSans',
+        fontSize: size,
+      );
   }
 
-  BoxDecoration boxDecoration(Color col, double rad, double borWid, Color borCol) {
+  BoxDecoration boxDecoration(
+      Color col, double rad, double borWid, Color borCol) {
     return BoxDecoration(
-      color: col,
-      borderRadius: BorderRadius.circular(rad),
-      border: Border.all(
-        color: borCol,
-        width: borWid
-      )
-    );
+        color: col,
+        borderRadius: BorderRadius.circular(rad),
+        border: Border.all(color: borCol, width: borWid));
   }
 }
