d368da15d46e3c78b59b0efa481e4dc4425bce86
Belk Library Staff <staffbl@LIC5070-70M6Z23.appd.appstate.edu>
2022-01-27 11:30:07 -0500

    feat: pool token boxes and half of details

--- lib/pages/DesktopPool.dart
+++ lib/pages/DesktopPool.dart
@@ -61,8 +61,7 @@ class _DesktopPoolState extends State<DesktopPool> {
   Widget allLiquidity() {
     double _width = MediaQuery.of(context).size.width;
     double _height = MediaQuery.of(context).size.height;
-    
-    BoxDecoration decor = boxDecoration(Colors.grey[800]!, 100, 0, Colors.grey[800]!);
+    double wid = _width*0.75;
 
     Widget toggle = Container(
       width: 260,
@@ -78,7 +77,7 @@ class _DesktopPoolState extends State<DesktopPool> {
               child: TextButton(
                 onPressed: () {},
                 child: Text("Add Liquidity",
-                style: textStyle(Colors.white, 16, true, false)
+                style: textStyle(Colors.white, 16, true)
               )
             )
           ),
@@ -93,7 +92,7 @@ class _DesktopPoolState extends State<DesktopPool> {
               },
               child: Text(
                 "My Liquidity",
-                style: textStyle(Colors.white, 16, true, false)
+                style: textStyle(Colors.white, 16, true)
               )
             )
           )
@@ -110,122 +109,164 @@ class _DesktopPoolState extends State<DesktopPool> {
           alignment: Alignment.bottomLeft,
           child: Text(
             "Liquidity Pool",
-            style: textStyle(Colors.white, 24, true, false)
+            style: textStyle(Colors.white, 24, true)
           )
         ),
         toggle,
         Container(
-          width: _width*0.75,
+          width: wid,
           height: 300,
           decoration: boxDecoration(Colors.grey[800]!.withOpacity(0.6), 30, 0.5, Colors.grey[400]!),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
               // Pool tokens side (add liq.)
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Container(
-                    child: Column(
+              Container(
+                height: 250,
+                width: wid/2-20,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: <Widget>[
                         // Top balance text 
-                        Container(),
-                        // Top Token container
                         Container(
-                          width: 400,
-                          decoration: boxDecoration(Colors.transparent, 20, .5, Colors.grey[400]!),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          alignment: Alignment.bottomRight,
+                          padding: EdgeInsets.only(right: 30),
+                          child: Text(
+                            "Balance: 0.00",
+                            style: textStyle(Colors.grey[600]!, 13, false)
+                          )
+                        ),
+                        // Top Token container
+                        tokenContainer(1),
+                      ],
+                    ),
+                    Container(
+                      child: Text(
+                        "+",
+                        style: textStyle(Colors.grey[600]!, 42, true),
+                      )
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        // Bottom balance text 
+                        Container(
+                          alignment: Alignment.bottomRight,
+                          padding: EdgeInsets.only(right: 30),
+                          child: Text(
+                            "Balance: 0.00",
+                            style: textStyle(Colors.grey[600]!, 13, false)
+                          )
+                        ),
+                        // Bottom Token container
+                        tokenContainer(2),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              // Pool details side (add liq.)
+              Container(
+                width: wid/2-20,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Details: Price and Pool Share",
+                        style: textStyle(Colors.white, 21, true),
+                      )
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        // AX per / APT per
+                        Container(
+                          child: Column(
                             children: <Widget>[
-                              // left-half of token box (token)
                               Container(
-                                width: 175,
-                                height: 40,
-                                decoration: decor,
-                                child: TextButton(
-                                  onPressed: () {},
-                                  // onPressed: () => showDialog(
-                                  //   context: context,
-                                  //   builder: (BuildContext context) => AthleteTokenList(context, tknNum, createTokenElement)),
-                                  // ),
-                                  child: Container(
-                                    width: 145,
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Container(
-                                          width: 30,
-                                          height: 30,
-                                          decoration: BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            // image: DecorationImage(
-                                            //   image: tokenImage!,
-                                            //   fit: BoxFit.fill,
-                                            // ),
-                                          ),
-                                        ),
-                                        Container(width: 10),
-                                        Expanded(
-                                          child: Text("todo", style: textStyle(Colors.white, 16, true, false)),
-                                        ), 
-                                        Icon(Icons.keyboard_arrow_down, color: Colors.white, size: 25)
-                                      ],
-                                    )
-                                  )
-                                )
-                              ),
-                              // right-half of token box
-                              Container(
+                                width: (wid/2-20)/2-20,
                                 child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: <Widget>[
-                                    Container(
-                                      height: 24,
-                                      width: 40,
-                                      decoration: boxDecoration(Colors.transparent, 100, 0.5, Colors.grey[400]!),
-                                      child: TextButton(
-                                        onPressed: () {
-                                          swapController.activeTkn1.value;
-                                          print(swapController.amount1);
-                                        },
-                                        child: Text(
-                                          "MAX",
-                                          style: textStyle(Colors.grey[400]!, 8, false, false)
-                                        )
-                                      )
+                                    Text(
+                                      "AX per tkn APT:",
+                                      style: textStyle(Colors.grey[600]!, 16, false),
                                     ),
-                                    SizedBox(
-                                      width: 70,
-                                      child: TextFormField(
-                                        onChanged: (value) {},
-                                        style: textStyle(Colors.grey[400]!, 22, false, false),
-                                        decoration: InputDecoration(
-                                          hintText: '0.00',
-                                          hintStyle: textStyle(Colors.grey[400]!, 22, false, false),
-                                          contentPadding: const EdgeInsets.all(9),
-                                          border: InputBorder.none,
-                                        ),
-                                        inputFormatters: [
-                                          FilteringTextInputFormatter.allow((RegExp(r'^(\d+)?\.?\d{0,2}'))),
-                                        ],
-                                      ),
-                                    ),
+                                    Text(
+                                      "2.24",
+                                      style: textStyle(Colors.grey[600]!, 16, false),
+                                    )
                                   ],
                                 )
-                              )
+                              ),
+                              Container(
+                                width: (wid/2-20)/2-20,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      "tkn APT per AX:",
+                                      style: textStyle(Colors.grey[600]!, 16, false),
+                                    ),
+                                    Text(
+                                      "1.48",
+                                      style: textStyle(Colors.grey[600]!, 16, false),
+                                    )
+                                  ],
+                                )
+                              ),
+                            ],
+                          )
+                        ),
+                        // pool share / exp. yield
+                        Container(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                width: (wid/2-20)/2-20,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      "Share of pool:",
+                                      style: textStyle(Colors.grey[600]!, 16, false),
+                                    ),
+                                    Text(
+                                      "0.12%",
+                                      style: textStyle(Colors.grey[600]!, 16, false),
+                                    )
+                                  ],
+                                )
+                              ),
+                              Container(
+                                width: (wid/2-20)/2-20,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      "Expected yield:",
+                                      style: textStyle(Colors.grey[600]!, 16, false),
+                                    ),
+                                    Text(
+                                      "24.12%",
+                                      style: textStyle(Colors.grey[600]!, 16, false),
+                                    )
+                                  ],
+                                )
+                              ),
                             ],
                           )
                         )
                       ],
-                    )
-                  )
-                ],
-              ),
-              // Pool details side (add liq.)
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  
-                ],
+                    ),
+                  ],
+                )
               ),
             ],
           )
@@ -257,7 +298,7 @@ class _DesktopPoolState extends State<DesktopPool> {
                   });
                 },
                 child: Text("Add Liquidity",
-                style: textStyle(Colors.white, 16, true, false)
+                style: textStyle(Colors.white, 16, true)
               )
             )
           ),
@@ -268,7 +309,7 @@ class _DesktopPoolState extends State<DesktopPool> {
               onPressed: () {},
               child: Text(
                 "My Liquidity",
-                style: textStyle(Colors.white, 16, true, false)
+                style: textStyle(Colors.white, 16, true)
               )
             )
           ),
@@ -286,7 +327,7 @@ class _DesktopPoolState extends State<DesktopPool> {
             alignment: Alignment.bottomLeft,
             child: Text(
               "My Liquidity",
-              style: textStyle(Colors.white, 24, true, false)
+              style: textStyle(Colors.white, 24, true)
             )
           ),
           toggle,
@@ -298,27 +339,104 @@ class _DesktopPoolState extends State<DesktopPool> {
     );
   }
 
-  TextStyle textStyle(Color color, double size, bool isBold, bool isUline) {
-    if (isBold) if (isUline)
-      return TextStyle(
-          color: color,
-          fontFamily: 'OpenSans',
-          fontSize: size,
-          fontWeight: FontWeight.w400,
-          decoration: TextDecoration.underline);
-    else
+  Widget tokenContainer(int which) {
+    BoxDecoration decor = boxDecoration(Colors.grey[800]!, 100, 0, Colors.grey[800]!);
+    
+    return Container(
+      height: 75,
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: boxDecoration(Colors.transparent, 20, .5, Colors.grey[400]!),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          // left-half of token box (token)
+          Container(
+            width: 175,
+            height: 40,
+            decoration: decor,
+            child: TextButton(
+              onPressed: () {},
+              // onPressed: () => showDialog(
+              //   context: context,
+              //   builder: (BuildContext context) => AthleteTokenList(context, tknNum, createTokenElement)),
+              // ),
+              child: Container(
+                width: 145,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        // image: DecorationImage(
+                        //   image: tokenImage!,
+                        //   fit: BoxFit.fill,
+                        // ),
+                      ),
+                    ),
+                    Container(width: 10),
+                    Expanded(
+                      child: Text("todo", style: textStyle(Colors.white, 16, true)),
+                    ), 
+                    Icon(Icons.keyboard_arrow_down, color: Colors.white, size: 25)
+                  ],
+                )
+              )
+            )
+          ),
+          // right-half of token box
+          Container(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  height: 24,
+                  width: 40,
+                  decoration: boxDecoration(Colors.transparent, 100, 0.5, Colors.grey[400]!),
+                  child: TextButton(
+                    onPressed: () {
+                      swapController.activeTkn1.value;
+                      print(swapController.amount1);
+                    },
+                    child: Text(
+                      "MAX",
+                      style: textStyle(Colors.grey[400]!, 8, false)
+                    )
+                  )
+                ),
+                SizedBox(
+                  width: 70,
+                  child: TextFormField(
+                    onChanged: (value) {},
+                    style: textStyle(Colors.grey[400]!, 22, false),
+                    decoration: InputDecoration(
+                      hintText: '0.00',
+                      hintStyle: textStyle(Colors.grey[400]!, 22, false),
+                      contentPadding: const EdgeInsets.all(9),
+                      border: InputBorder.none,
+                    ),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow((RegExp(r'^(\d+)?\.?\d{0,2}'))),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          )
+        ],
+      )
+    );
+  }
+
+  TextStyle textStyle(Color color, double size, bool isBold) {
+    if (isBold) 
       return TextStyle(
         color: color,
         fontFamily: 'OpenSans',
         fontSize: size,
         fontWeight: FontWeight.w400,
       );
-    else if (isUline)
-      return TextStyle(
-          color: color,
-          fontFamily: 'OpenSans',
-          fontSize: size,
-          decoration: TextDecoration.underline);
     else
       return TextStyle(
         color: color,
