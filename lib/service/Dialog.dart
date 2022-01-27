2b2273b137f04b70bbd2894f3c64eba2286e452a
mretana1999 <mretana1999@gmail.com>
2022-01-27 16:47:20 -0500

    chore: fixed typos and colons in yourAXDialog (wallet dashboard)

--- lib/service/Dialog.dart
+++ lib/service/Dialog.dart
@@ -2261,7 +2261,7 @@ Dialog yourAXDialog(BuildContext context) {
                           children: <Widget>[
                             Container(
                               child: Text(
-                                "AX in circulation",
+                                "AX in circulation:",
                                 style: TextStyle(
                                   fontSize: 15,
                                   color: Colors.grey[600],
