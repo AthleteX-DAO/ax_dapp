c8ee8c8e4180c63e443b328e7553f138d1aa5d7b
Jakhongir Bekchanov <bekchanovj@gmail.com>
2022-02-12 22:45:38 -0500

    <test> Testing code for minting

--- lib/service/Controller/Scout/LSPController.dart
+++ lib/service/Controller/Scout/LSPController.dart
@@ -3,8 +3,10 @@
 import 'package:ax_dapp/service/Controller/APT.dart';
 import 'package:get/get.dart';
 import 'package:web3dart/credentials.dart';
+import 'package:web3dart/web3dart.dart';
 import '../../../contracts/LongShortPair.g.dart';
 import 'package:web3dart/contracts/erc20.dart';
+import 'package:http/http.dart';
 import '../Controller.dart';
 
 // --> Joe burrow
@@ -19,30 +21,43 @@ class LSPController extends GetxController {
   var redeemAmt = 0.0.obs;
   // Hard address listing of all Athletes
 
-  LSPController() {
-    final theClient = controller.client.value;
-    EthereumAddress address =
-        EthereumAddress.fromHex("0xD3E03e36D70F65A00732F9086D994D83A3EaC286");
-    genericLSP = LongShortPair(address: address, client: theClient);
-  }
+  LSPController();
 
   Future<void> mint() async {
+    //final theClient =        Web3Client("https://matic-mumbai.chainstacklabs.com", Client());
+    final tokenClient =
+        Web3Client("https://matic-mumbai.chainstacklabs.com", Client());
+    EthereumAddress address =
+        EthereumAddress.fromHex("0xD3E03e36D70F65A00732F9086D994D83A3EaC286");
+    genericLSP =
+        LongShortPair(address: address, client: controller.client.value);
     print("Attempting to MINT LSP");
+    print(await controller.credentials.extractAddress());
     final theCredentials = controller.credentials;
     BigInt tokensToCreate = BigInt.from(createAmt.value);
-    
+
     approve().then((value) async {
-    String txString =
-        await genericLSP.create(tokensToCreate, credentials: theCredentials);
-    controller.updateTxString(txString); //Sends tx to controller
+      String txString =
+          await genericLSP.create(tokensToCreate, credentials: theCredentials);
+      controller.updateTxString(txString); //Sends tx to controller
     });
   }
 
   Future<void> approve() async {
+    var amount;
+    print("[Console] Inside approve()");
     EthereumAddress address =
         EthereumAddress.fromHex("0x76d9a6e4cdefc840a47069b71824ad8ff4819e85");
-    Erc20 axt = Erc20(address: address, client: controller.client.value);
-    BigInt amount = await genericLSP.collateralPerPair();
+    final tokenClient =
+        Web3Client("https://matic-mumbai.chainstacklabs.com", Client());
+    Erc20 axt = Erc20(address: address, client: tokenClient);
+    print("[Console] Created a token variable.");
+    try {
+      amount = await genericLSP.collateralPerPair();
+    } catch (error) {
+      print(error);
+    }
+    print("[Console] Got the amount");
     EthereumAddress spender =
         EthereumAddress.fromHex("0xD3E03e36D70F65A00732F9086D994D83A3EaC286");
     String txString =
