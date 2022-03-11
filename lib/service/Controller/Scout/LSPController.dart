5245c7a194e92657b17b549d10024756830630ed
Jakhongir Bekchanov <bekchanovj@gmail.com>
2022-03-11 15:36:59 -0500

    <refactor> Clean up

--- lib/service/Controller/Scout/LSPController.dart
+++ lib/service/Controller/Scout/LSPController.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
+import 'dart:io';
+
 import 'package:ax_dapp/service/Controller/Swap/AXT.dart';
 import 'package:ax_dapp/service/Controller/APT.dart';
 import 'package:get/get.dart';
@@ -30,52 +32,49 @@ class LSPController extends GetxController {
     EthereumAddress address = EthereumAddress.fromHex(aptAddress.value);
     genericLSP = LongShortPair(address: address, client: tokenClient);
     print("Attempting to MINT LSP");
-    print(await controller.credentials.extractAddress());
     final theCredentials = controller.credentials;
     BigInt tokensToCreate =
         BigInt.from(createAmt.value) * BigInt.from(1000000000000000000);
-
-    // approve().then((value) async {
-    // });
-    approve().then((value) async {
+    approve(genericLSP, address).then((value) async {
       String txString =
           await genericLSP.create(tokensToCreate, credentials: theCredentials);
       controller.updateTxString(txString); //Sends tx to controller
     });
   }
 
-  Future<void> approve() async {
+  Future<void> approve(
+      LongShortPair genericLSP, EthereumAddress address) async {
     print("collaterall amount: ${await genericLSP.collateralPerPair()}");
-    EthereumAddress address = EthereumAddress.fromHex(aptAddress.value);
-    genericLSP = LongShortPair(address: address, client: tokenClient);
-    BigInt transferAmount = BigInt.parse("1000000000000000000000000000");
+    BigInt transferAmount = await genericLSP.collateralPerPair();
     BigInt amount = BigInt.from(createAmt.value) * transferAmount;
     print("[Console] Inside approve()");
     EthereumAddress axtaddress = EthereumAddress.fromHex(AXT.mumbaiAddress);
     Erc20 axt = Erc20(address: axtaddress, client: tokenClient);
     try {
-      print("[Console] Created a token variable.");
-    } catch (error) {
-      print(error);
+      await axt.approve(address, amount, credentials: controller.credentials);
+    } catch (e) {
+      print("[Console] Could not approve: $e");
     }
-    EthereumAddress spender = EthereumAddress.fromHex(aptAddress.value);
-    String txString =
-        await axt.approve(address, amount, credentials: controller.credentials);
   }
 
   Future<void> redeem() async {
     EthereumAddress address = EthereumAddress.fromHex(aptAddress.value);
     genericLSP = LongShortPair(address: address, client: tokenClient);
     final theCredentials = controller.credentials;
-    BigInt tokensToRedeem = BigInt.from(redeemAmt.value);
+    BigInt tokensToRedeem =
+        BigInt.from(redeemAmt.value) * BigInt.from(1000000000000000000);
     genericLSP.redeem(tokensToRedeem, credentials: theCredentials);
   }
 
   void updateAptAddress(int athleteId) {
     if (AXT.idToAddress.containsKey(athleteId)) {
       aptAddress.value = AXT.idToAddress[athleteId] as String;
+      print("[Console] Updated the aptAddress to $aptAddress");
+    } else {
+      aptAddress.value = '';
+      throw Exception("Player id is not supported!");
     }
-    print("[Console] Updated the aptAddress to $aptAddress");
+
     update();
   }
 
