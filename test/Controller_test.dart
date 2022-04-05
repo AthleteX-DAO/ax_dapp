bb716abec87238a87ecb535894075433593ea00a
Kevin Kamto <kamtokevin@gmail.com>
2022-04-04 22:51:44 -0500

    refactor: stripped unit test

--- test/Controller_test.dart
+++ test/Controller_test.dart
@@ -8,17 +8,6 @@ void main() async {
       "update elbow source spin squeeze horror world become oak assist bomb nuclear";
   String anInvalidMnemonic = "potatoe kendrick sparks";
 
-  bool returnFalseBoolean = await controller.importMnemonic(anInvalidMnemonic);
-  bool returnTrueBoolean = await controller.importMnemonic(aValidMnemonic);
-  test('Testing Invalid Mnemonic', () {
-    expect(returnFalseBoolean, false);
-  });
-
-  test('Testing Valid Mnemonic', () {
-    expect(returnTrueBoolean, true);
-  });
-
-  controller.connectNative();
   test('Testing if valid native connection is made', () {
     expect(controller.networkID.value, 80001);
   });
