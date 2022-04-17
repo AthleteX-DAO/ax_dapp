f3d3c61905ffc2f3ec4e5cdd87a2ff1cf2c62088
Ryan Bharath <dbossman60@gmail.com>
2022-04-16 23:46:29 -0400

    feat: added the rest of the MLB positions and mapped them to their full names

--- lib/util/AbbreviationMappingsHelper.dart
+++ lib/util/AbbreviationMappingsHelper.dart
@@ -1,6 +1,17 @@
 // ignore_for_file: non_constant_identifier_names
 
-Map<String, String> _MLBAthleteMappingPosition = {'RF' : 'Right Fielder', 'SS' : 'Short Stop', '1B' : 'First Baseman', '3B' : 'Third Baseman', 'CF' : 'Center Fielder', 'LF' : 'Left Fielder'};
+Map<String, String> _MLBAthleteMappingPosition = {
+  'RF' : 'Right Fielder', 
+  'SS' : 'Short Stop', 
+  '1B' : 'First Baseman', 
+  '2B' : 'Second Baseman', 
+  '3B' : 'Third Baseman', 
+  'CF' : 'Center Fielder', 
+  'LF' : 'Left Fielder', 
+  'RP' : 'Relief Pitcher', 
+  'SP' : 'Starting Pitcher', 
+  'C' : 'Catcher', 
+  'DH' : 'Designated Hitter'};
 
 String retrieveFullMLBAthletePosition(String currentMLBAthletePositon) {
   if(_MLBAthleteMappingPosition.containsKey(currentMLBAthletePositon)) {
