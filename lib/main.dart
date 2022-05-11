e9b11a11e66e0f7824cc00cc7f3451ed36fa7e92
Jakhongir Bekchanov <bekchanovj@gmail.com>
2022-05-10 20:44:38 -0400

    fix: deleting unused import

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:ax_dapp/repositories/usecases/GetPairInfoUseCase.dart';
 import 'package:ax_dapp/service/Api/MLBAthleteAPI.dart';
 import 'package:ax_dapp/service/GraphQL/GraphQLClientHelper.dart';
 import 'package:ax_dapp/service/GraphQL/GraphQLConfiguration.dart';
-import 'package:ax_dapp/service/athlete_api/MLBAthleteAPI.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
