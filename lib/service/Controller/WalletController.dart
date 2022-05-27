651ddca40044a7e15548961328c757d7feb25ce6
Kevin Kamto <kamtokevin@gmail.com>
2022-05-27 13:44:00 -0400

    chore: fix the build error

--- lib/service/Controller/WalletController.dart
+++ lib/service/Controller/WalletController.dart
@@ -91,8 +91,9 @@ class WalletController extends GetxController {
   }
 
   void buyAX() {
-    String axEth =
-        "https://app.sushi.com/swap?inputCurrency=0x5617604ba0a30e0ff1d2163ab94e50d8b6d0b0df&outputCurrency=0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
+    // TODO: Update this when we need sushiswap connection
+    // String axEth =
+    //     "https://app.sushi.com/swap?inputCurrency=0x5617604ba0a30e0ff1d2163ab94e50d8b6d0b0df&outputCurrency=0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
     String axEthUniswap = "https://app.uniswap.org/#/swap?chain=polygon";
 
     launchUrl(Uri.parse(axEthUniswap));
