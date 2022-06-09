1537d52ba8e99ddb2d63f7122bd3de351eed7a45
MichaelFrank0317 <michaelfrank0317@outlook.com>
2022-06-09 15:19:20 +0000

    fix: accurate book price for long and short token on scout and athlete page

--- lib/pages/scout/models/MarketModel.dart
+++ lib/pages/scout/models/MarketModel.dart
@@ -1,11 +1,18 @@
 class MarketModel {
   final double marketPrice;
   final double percentage;
+  final double bookPrice;
 
-  MarketModel({required double marketPrice, required double recentPrice}) :
-    this.marketPrice = marketPrice,
-    this.percentage = recentPrice > 0 ? (marketPrice - recentPrice) * 100 / recentPrice : 0.0;
-    
-    // this purpose of this line is to generate some fake data because we don't have any history of price at the moment
-    // this.percentage = new Random().nextDouble() > 0.5 ? new Random().nextDouble() : -1 * new Random().nextDouble();
+  MarketModel(
+      {required double marketPrice,
+      required double recentPrice,
+      required double bookPrice})
+      : this.marketPrice = marketPrice,
+        this.percentage = recentPrice > 0
+            ? (marketPrice - recentPrice) * 100 / recentPrice
+            : 0.0,
+        this.bookPrice = bookPrice;
+
+  // this purpose of this line is to generate some fake data because we don't have any history of price at the moment
+  // this.percentage = new Random().nextDouble() > 0.5 ? new Random().nextDouble() : -1 * new Random().nextDouble();
 }
