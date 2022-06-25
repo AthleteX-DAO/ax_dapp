8d26602cc9f1589bc023e309c55597a664abacc2
Ryan Bharath <dbossman60@gmail.com>
2022-06-25 02:06:38 -0400

    style: uncommented some code

--- lib/pages/pool/AddLiquidity/AddLiquidity.dart
+++ lib/pages/pool/AddLiquidity/AddLiquidity.dart
@@ -382,7 +382,7 @@ class _AddLiquidityState extends State<AddLiquidity> {
 
         Widget youWillReceiveToolTip() {
           return Tooltip(
-            //height: 50,
+            height: 50,
             padding: EdgeInsets.all(10),
             verticalOffset: -60,
             decoration: BoxDecoration(
