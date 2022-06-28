ad80ccdcba52726911f8537567207d2f2e8101ab
Pearlson Omesa <Pearlson.omesa@gmail.com>
2022-06-28 16:46:10 -0500

    created local variable for from and until and changed the until date to date.now. Also remove unused code, Also fixed market price text being off to the left

--- lib/pages/athlete/AthletePage.dart
+++ lib/pages/athlete/AthletePage.dart
@@ -1417,7 +1417,7 @@ class _AthletePageState extends State<AthletePage> {
                                   width: 200,
                                   child: Row(
                                       mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
+                                          MainAxisAlignment.end,
                                       children: <Widget>[
                                     Text(
                                         (_longAptIndex == 0)
@@ -1425,8 +1425,8 @@ class _AthletePageState extends State<AthletePage> {
                                             : "${athlete.shortTokenPrice!.toStringAsFixed(2)} AX",
                                         style: textStyle(
                                             Colors.white, 14, false, false)),
-                                    Container(width: 5),
                                     Container(
+                                      margin: EdgeInsets.only(left: 2),
                                         child: Text(
                                             (_longAptIndex == 0)
                                                 ? getPercentageDesc(athlete
