b9b25165498c1a0d63fcb93aea305a159ac87844
Rolly <rolly@narcodic.com>
2022-07-16 18:36:16 +0300

    fix: lints on coin api

--- lib/service/CoinApi.dart
+++ lib/service/CoinApi.dart
@@ -1,27 +1,26 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'Coin.dart';
+
+import 'package:ax_dapp/service/Coin.dart';
 import 'package:http/http.dart' as http;
 
 class CoinApi {
-  Future<List<Coin>> getCoins(BuildContext context) async {
-    var allData = await http.get(Uri.parse(
-        "https://rest.coinapi.io/v1/assets?apikey=8E8E1331-682E-4FAF-8E61-5D658F7B9AD4"));
-    var allBody = json.decode(allData.body);
+  Future<List<Coin>> getCoins() async {
+    final data = await http.get(
+      Uri.parse(
+        'https://rest.coinapi.io/v1/assets?apikey=8E8E1331-682E-4FAF-8E61-5D658F7B9AD4',
+      ),
+    );
+    final coinsJson = json.decode(data.body) as List<dynamic>;
 
     // final assetBundle = DefaultAssetBundle.of(context);
     // final ourData = await assetBundle.loadString('assets/coins.json');
     // final ourBody = json.decode(ourData);
 
-    var tickerList = ["ETH", "WBTC", "AX", "USDC", "DAI"];
+    final tickerList = ['ETH', 'WBTC', 'AX', 'USDC', 'DAI'];
 
-    var allList = allBody.map<Coin>(Coin.fromJson).toList();
-    List<Coin> ourList = [];
-
-    for (Coin coin in allList) {
-      if (tickerList.contains(coin.ticker)) ourList.add(coin);
-    }
-
-    return ourList;
+    final coins = List<Map<String, dynamic>>.from(coinsJson)
+        .map<Coin>(Coin.fromJson)
+        .toList();
+    return coins.where((coin) => tickerList.contains(coin.ticker)).toList();
   }
 }
