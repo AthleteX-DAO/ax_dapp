1f47907afd41ff716d2734c7122bad3cc1ae535e
Rolly <rolly@narcodic.com>
2022-07-16 16:27:39 +0300

    refactor: add super key on main

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
