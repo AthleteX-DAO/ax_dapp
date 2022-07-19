a4f75f24801f7448e8ec07efb7a276160959117d
william980913 <99713237+william980913@users.noreply.github.com>
2022-07-19 16:50:25 -0400

    [feat] added question icon on bottom right (#472)
    * [feat] added question icon on bottom right
    
    * feat: added toottip for help button and redirects to help site
    
    * feat: update link for help
    
    * fix: remove V1App
    
    * fix: lint errors
    
    * requested changes
    
    Co-authored-by: Pearlson Omesa <Pearlson.omesa@gmail.com>

---
+++ lib/pages/footer/simple_tool_tip.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppToolTip extends StatelessWidget {
+  const AppToolTip(this.message, this.child, {super.key});
+  final String message;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+      triggerMode: TooltipTriggerMode.tap,
+      showDuration: Duration.zero,
+      height: 20,
+      padding: const EdgeInsets.all(20),
+      preferBelow: true,
+      decoration: BoxDecoration(
+        color: Colors.grey[800],
+        borderRadius: BorderRadius.circular(50),
+      ),
+      richMessage: TextSpan(
+        children: <TextSpan>[
+          TextSpan(
+              text: message,
+              style: TextStyle(color: Colors.grey[400], fontSize: 16),),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
