f007bd0e435d10eb143d3ed6c7bc5860a3059d70
Rolly <rolly@narcodic.com>
2022-07-20 00:18:14 +0300

    feat: add ethereum_api package

---
+++ packages/ethereum_api/lib/ethereum_api.dart
@@ -0,0 +1,3 @@
+library ethereum_api;
+
+export 'src/ethereum_api_client.dart';
---
+++ packages/ethereum_api/lib/src/ethereum_api_client.dart
@@ -0,0 +1,7 @@
+/// {@template ethereum_api_client}
+/// API client which handles interaction with the Ethereum blockchain.
+/// {@endtemplate}
+class EthereumApiClient {
+  /// {@macro ethereum_api_client}
+  const EthereumApiClient();
+}
