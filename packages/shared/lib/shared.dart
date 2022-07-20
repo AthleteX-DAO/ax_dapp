85b2f110d6f74a2fab22a15bc4023ee5cfb7afce
Rolly <rolly@narcodic.com>
2022-07-21 00:24:39 +0300

    chore: remove pure dart deps from pubspec

--- packages/shared/lib/shared.dart
+++ packages/shared/lib/shared.dart
@@ -1,1 +1,13 @@
 library shared;
+
+export 'package:big_decimal/big_decimal.dart';
+export 'package:bip39/bip39.dart';
+export 'package:bloc/bloc.dart';
+export 'package:decimal/decimal.dart';
+export 'package:dio/dio.dart';
+export 'package:equatable/equatable.dart';
+export 'package:erc20/erc20.dart';
+export 'package:fpdart/fpdart.dart';
+export 'package:gql/language.dart';
+export 'package:http/http.dart' hide MultipartFile, Response;
+export 'package:json_annotation/json_annotation.dart';
