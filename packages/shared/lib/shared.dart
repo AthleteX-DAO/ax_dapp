a6d948308f78adef6db6340326b9c7606bc8dfbc
Rolly <rolly@narcodic.com>
2022-07-21 17:07:01 +0300

    chore(shared): export additional libraries

--- packages/shared/lib/shared.dart
+++ packages/shared/lib/shared.dart
@@ -11,3 +11,6 @@ export 'package:fpdart/fpdart.dart';
 export 'package:gql/language.dart';
 export 'package:http/http.dart' hide MultipartFile, Response;
 export 'package:json_annotation/json_annotation.dart';
+export 'package:web3_browser/web3_browser.dart';
+export 'package:web3dart/web3dart.dart';
+export 'package:web_socket_channel/web_socket_channel.dart';
