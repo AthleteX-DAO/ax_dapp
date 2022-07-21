605e988403f09200b5227a2357a8bd378a4e8858
Rolly <rolly@narcodic.com>
2022-07-22 02:48:36 +0300

    fix(shared): remove problematic exports

--- packages/shared/lib/shared.dart
+++ packages/shared/lib/shared.dart
@@ -9,8 +9,8 @@ export 'package:equatable/equatable.dart';
 export 'package:erc20/erc20.dart';
 export 'package:fpdart/fpdart.dart';
 export 'package:gql/language.dart';
-export 'package:http/http.dart' hide MultipartFile, Response;
 export 'package:json_annotation/json_annotation.dart';
-export 'package:web3_browser/web3_browser.dart';
 export 'package:web3dart/web3dart.dart';
 export 'package:web_socket_channel/web_socket_channel.dart';
+
+export 'src/failure/failure.dart';
