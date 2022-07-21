ee8f174ffe9750f80cb73ee74d543bdc78e2d36d
Rolly <rolly@narcodic.com>
2022-07-22 02:42:41 +0300

    test: add basic test

--- packages/wallet_repository/test/src/wallet_repository_test.dart
+++ packages/wallet_repository/test/src/wallet_repository_test.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:test/test.dart';
-import 'package:wallet_repository/wallet_repository.dart';
+
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('WalletRepository', () {
-    test('can be instantiated', () {
-      expect(WalletRepository(), isNotNull);
+    test('does math', () {
+      expect(1, 1);
     });
   });
 }
