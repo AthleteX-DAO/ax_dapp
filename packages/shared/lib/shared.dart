ceae7d21ccea173f462ca803c06c100461791af4
Rolly <rolly@narcodic.com>
2022-07-23 01:47:31 +0300

    chore(shared): add rxdart and collection

--- packages/shared/lib/shared.dart
+++ packages/shared/lib/shared.dart
@@ -3,6 +3,7 @@ library shared;
 export 'package:big_decimal/big_decimal.dart';
 export 'package:bip39/bip39.dart';
 export 'package:bloc/bloc.dart';
+export 'package:collection/collection.dart';
 export 'package:decimal/decimal.dart';
 export 'package:dio/dio.dart';
 export 'package:equatable/equatable.dart';
@@ -10,6 +11,7 @@ export 'package:erc20/erc20.dart';
 export 'package:fpdart/fpdart.dart';
 export 'package:gql/language.dart';
 export 'package:json_annotation/json_annotation.dart';
+export 'package:rxdart/rxdart.dart';
 export 'package:web3dart/web3dart.dart';
 export 'package:web_socket_channel/web_socket_channel.dart';
 
