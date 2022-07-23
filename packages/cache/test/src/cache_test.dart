2c854651205358e51b4c9efdbe9f1f4723fa9a9c
Rolly <rolly@narcodic.com>
2022-07-23 21:10:17 +0300

    feat: add `cache` package

---
+++ packages/cache/test/src/cache_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:cache/cache.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('CacheClient', () {
+    test('can be instantiated', () {
+      expect(CacheClient(), isNotNull);
+    });
+  });
+}
