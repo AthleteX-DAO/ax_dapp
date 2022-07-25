6014211eb472a04986151e90e658dd0fb970e2e6
pmd <info@powermoves.io>
2022-07-24 23:21:03 -0400

    no-jira: add release and staging github actions

---
+++ launch_container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Container launch sequence..."
+echo " * FETCHING LATEST CHANGES"
+git fetch
+echo " * PULL LATEST RELEASE"
+git pull
+docker-compose pull
+docker-compose stop
+echo " * CLEANING OLD IMAGES"
+docker-compose rm -f
+echo " * RESTART CONTAINER WITH LATEST IMAGE"
+sudo docker-compose up -d
+echo " * PRUNE OLD IMAGES"
+docker image prune -af
