d249d1263634605ec7708137f8f4526e71cd953c
Rolly <rolly@narcodic.com>
2022-07-26 04:28:41 +0300

    feat: implement wallet methods

--- packages/wallet_repository/lib/src/wallet_repository.dart
+++ packages/wallet_repository/lib/src/wallet_repository.dart
@@ -1,34 +1,76 @@
-import 'package:flutter_web3/flutter_web3.dart';
-import 'package:wallet_repository/src/models/models.dart';
+import 'dart:async';
+
+import 'package:cache/cache.dart';
+import 'package:ethereum_api/ethereum_api.dart';
+import 'package:shared/shared.dart';
 
 /// {@template wallet_repository}
 /// Repository that manages the wallet domain.
 /// {@endtemplate}
 class WalletRepository {
   /// {@macro wallet_repository}
-  WalletRepository({Ethereum? ethereum})
-      : _ethereum = ethereum ?? Ethereum.provider;
+  WalletRepository({
+    WalletApiClient? walletApiClient,
+    CacheClient? cache,
+    required EthereumChain seedChain,
+  })  : _walletApiClient = walletApiClient ?? EthereumWalletApiClient(),
+        _cache = cache ?? CacheClient(),
+        _seedChain = seedChain;
 
-  final Ethereum? _ethereum;
+  final WalletApiClient _walletApiClient;
+  final CacheClient _cache;
+
+  /// The initial `EthereumChain` that the wallet will be switched to.
+  final EthereumChain _seedChain;
+
+  /// [Credentials] cache key.
+  /// Should only be used for testing purposes.
+  @visibleForTesting
+  static const credentialsCacheKey = '__credentials_cache_key__';
+
+  /// Allows listening to changes to the current [EthereumChain].
+  Stream<EthereumChain> get ethereumChainChanges =>
+      _walletApiClient.ethereumChainChanges;
+
+  /// Returns the current [EthereumChain] synchronously.
+  EthereumChain get ethereumChain => _walletApiClient.ethereumChain;
 
   /// Allows the user to connect to a `MetaMask` wallet.
-  Future<void> connectWallet() {
-    throw UnimplementedError();
+  Future<void> connectWallet() async {
+    _walletApiClient.addChainChangedListener();
+    await switchChain(_seedChain);
+    await _getWalletCredentials();
   }
 
   /// Switches the currently used chain.
-  Future<void> switchChain(EthereumChain ethereumChain) async {
-    _checkEthereumAvailability();
+  Future<void> switchChain(EthereumChain chain) async {
     try {
-      await _ethereum!.walletSwitchChain(ethereumChain.chainId);
-    } catch (e) {
-      //
+      await _walletApiClient.switchChain(chain);
+    } on WalletUnrecognizedChainFailure {
+      await _walletApiClient.addChain(chain);
     }
   }
 
-  void _checkEthereumAvailability() {
-    if (_ethereum == null) {
-      throw UnimplementedError();
-    }
+  Future<void> _getWalletCredentials() async {
+    final credentials = await _walletApiClient.getWalletCredentials();
+    _cacheWalletCredentials(credentials);
   }
+
+  void _cacheWalletCredentials(WalletCredentials credentials) => _cache.write(
+        key: credentialsCacheKey,
+        value: credentials,
+      );
+
+  /// Returns the cached [WalletCredentials] for the connected wallet. This
+  /// doesn't return `null`, because when called, the wallet is asssumed to be
+  /// connected and thus have it's credentials cached.
+  ///
+  /// This can used in the BLoC layer to pass [WalletCredentials] to other calls
+  /// needing them.
+  WalletCredentials get credentials =>
+      _cache.read<WalletCredentials>(key: credentialsCacheKey)!;
+
+  /// Simulates disconnecting user's wallet. For security reasons an actual
+  /// disconnect is not possible.
+  void disconnectWallet() => _walletApiClient.removeChainChangedListener();
 }
--- packages/wallet_repository/lib/wallet_repository.dart
+++ packages/wallet_repository/lib/wallet_repository.dart
@@ -1,3 +1,6 @@
 library wallet_repository;
 
+export 'package:ethereum_api/ethereum_api.dart'
+    hide EthereumApiClient, EthereumWalletApiClient, WalletApiClient;
+
 export 'src/wallet_repository.dart';
