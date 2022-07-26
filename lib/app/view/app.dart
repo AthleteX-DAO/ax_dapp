b72ed16c3beb793c36936a93f8322b6f608326e0
Rolly <rolly@narcodic.com>
2022-07-26 04:35:48 +0300

    feat: provide `WalletBloc`

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,23 +1,31 @@
 import 'package:ax_dapp/pages/landing_page/landing_page.dart';
+import 'package:ax_dapp/wallet/wallet.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wallet_repository/wallet_repository.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AthleteX',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      theme: ThemeData(
-        canvasColor: Colors.transparent,
-        brightness: Brightness.dark,
-        primaryColor: Colors.yellow[700],
-        colorScheme: ColorScheme.fromSwatch(brightness: Brightness.dark)
-            .copyWith(secondary: Colors.black),
+    return BlocProvider(
+      create: (_) => WalletBloc(
+        walletRepository: context.read<WalletRepository>(),
       ),
-      home: const LandingPage(),
+      child: MaterialApp(
+        title: 'AthleteX',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        theme: ThemeData(
+          canvasColor: Colors.transparent,
+          brightness: Brightness.dark,
+          primaryColor: Colors.yellow[700],
+          colorScheme: ColorScheme.fromSwatch(brightness: Brightness.dark)
+              .copyWith(secondary: Colors.black),
+        ),
+        home: const LandingPage(),
+      ),
     );
   }
 }
