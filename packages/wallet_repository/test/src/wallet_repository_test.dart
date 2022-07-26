df0d1cb49fa36a72f7e51ad7a767628d19f49246
Rolly <rolly@narcodic.com>
2022-07-26 04:29:20 +0300

    test(wallet_repository): add basic test

--- packages/wallet_repository/test/src/wallet_repository_test.dart
+++ packages/wallet_repository/test/src/wallet_repository_test.dart
@@ -1,11 +1,36 @@
+@TestOn('chrome')
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter_test/flutter_test.dart';
+import 'package:cache/cache.dart';
+import 'package:ethereum_api/ethereum_api.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+import 'package:wallet_repository/src/wallet_repository.dart';
+
+class MockWalletApiClient extends Mock implements WalletApiClient {}
+
+class MockCacheClient extends Mock implements CacheClient {}
 
 void main() {
   group('WalletRepository', () {
-    test('does math', () {
-      expect(1, 1);
+    late WalletApiClient walletApiClient;
+    late CacheClient cache;
+
+    late WalletRepository subject;
+
+    setUp(() {
+      walletApiClient = MockWalletApiClient();
+      cache = MockCacheClient();
+
+      subject = WalletRepository(
+        walletApiClient: walletApiClient,
+        cache: cache,
+        seedChain: EthereumChain.polygonMainnet,
+      );
+    });
+
+    test('can be instantiated', () {
+      expect(subject, isNotNull);
     });
   });
 }
