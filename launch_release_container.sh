56650703de7caaecd91d6fd7935911d7d5816260
Andrei Durnea <durduman@yahoo.com>
2022-07-27 21:55:34 +0300

    Revert "no-jira: use port arg value in docker compose file"
    This reverts commit 207db6ca41b918bc35625724746664778e37f82a.

--- launch_release_container.sh
+++ launch_release_container.sh
@@ -9,9 +9,8 @@ docker-compose stop
 echo " * CLEANING OLD IMAGES"
 docker-compose rm -f
 echo " * RESTART CONTAINER WITH LATEST IMAGE"
-export PORT=5000
 export BUILD_TYPE=release
-docker-compose build --build-arg PORT=5000 --build-arg BUILD_TYPE=release
-docker-compose up -d
+export PORT=5000
+sudo docker-compose up -d
 echo " * PRUNE OLD IMAGES"
 docker image prune -af
