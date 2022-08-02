baf310556eaa06e2d5b11f69f4dbd504446ebf3e
Ryan Bharath <dbossman60@gmail.com>
2022-08-02 16:10:06 -0400

    chore: formatting tracking_repository.dart

--- packages/tracking_repository/lib/src/tracking_repository.dart
+++ packages/tracking_repository/lib/src/tracking_repository.dart
@@ -3,39 +3,39 @@ import 'dart:developer' as dev;
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:tracking_repository/src/track_event.dart';
 
-
 /// Used to interface tracking services
 class TrackingRepository {
-/// Default construcot
-TrackingRepository({
-FirebaseAnalytics? firebase,
-}) : _firebase = firebase ?? FirebaseAnalytics.instance ;
-/// Update to configure with relevant trackers
-final FirebaseAnalytics _firebase;
+  /// Default construcot
+  TrackingRepository({
+    FirebaseAnalytics? firebase,
+  }) : _firebase = firebase ?? FirebaseAnalytics.instance;
 
-/// Will add events containg the complet updated list of events
-Stream<List<TrackEvent>> get loggedEventsStream =>
-_loggedEventsController.stream.asBroadcastStream();
-final _loggedEventsController = StreamController<List<TrackEvent>>();
-final _loggedEvents = <TrackEvent>[];
+  /// Update to configure with relevant trackers
+  final FirebaseAnalytics _firebase;
 
-void _updateLoggedEvents(TrackEvent event) {
-_loggedEvents.insert(0, event);
-_loggedEventsController.add(_loggedEvents);
-}
+  /// Will add events containg the complet updated list of events
+  Stream<List<TrackEvent>> get loggedEventsStream =>
+      _loggedEventsController.stream.asBroadcastStream();
+  final _loggedEventsController = StreamController<List<TrackEvent>>();
+  final _loggedEvents = <TrackEvent>[];
 
-///
-/// Packages/trackers may have additional lifecycle requirements.
-/// These methods like id/reset/consent and other ones should be handled here.
-///
-///
-/// Updates tracking services accordingly
-void track(TrackEvent event) {
-_firebase.logEvent(
-name: event.name,
-parameters: event.params,
-);
-dev.log(event.name);
-_updateLoggedEvents(event);
-}
+  void _updateLoggedEvents(TrackEvent event) {
+    _loggedEvents.insert(0, event);
+    _loggedEventsController.add(_loggedEvents);
+  }
+
+  ///
+  /// Packages/trackers may have additional lifecycle requirements.
+  /// These methods like id/reset/consent and other ones should be handled here.
+  ///
+  ///
+  /// Updates tracking services accordingly
+  void track(TrackEvent event) {
+    _firebase.logEvent(
+      name: event.name,
+      parameters: event.params,
+    );
+    dev.log(event.name);
+    _updateLoggedEvents(event);
+  }
 }
