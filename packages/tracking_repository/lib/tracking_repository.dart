faf75060df6d625a150e1bb80eda6d95b14900f2
Andrei Durnea <durduman@yahoo.com>
2022-08-03 18:07:21 +0300

    fixed compilation

--- packages/tracking_repository/lib/tracking_repository.dart
+++ packages/tracking_repository/lib/tracking_repository.dart
@@ -4,5 +4,6 @@ export 'src/athlete_page_events.dart';
 export 'src/landing_page_events.dart';
 export 'src/pool_page_events.dart';
 export 'src/scout_page_events.dart';
+export 'src/track_event.dart';
 export 'src/tracking_repository.dart';
 export 'src/trade_page_events.dart';
