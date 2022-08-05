1bc84840ad1c87427485d6fa558a51ac928d7d0e
MichaelFrank0317 <michaelfrank0317@outlook.com>
2022-08-05 21:22:21 +0000

    feat: farm page tracking events

---
+++ packages/tracking_repository/lib/src/farm_page_events.dart
@@ -0,0 +1,45 @@
+import 'package:tracking_repository/src/track_event.dart';
+
+/// Default events available on the Athlete Page
+class FarmPageTrackingEvent extends TrackEvent {
+  /// Informs tracking services that stake button was pressed
+  FarmPageTrackingEvent.onPressedStake(
+      Map<String, dynamic> params,
+      ) :super(name: 'stake_approve_button_click', params: params);
+
+  /// Informs tracking services that confirmed stake
+  FarmPageTrackingEvent.onPressedStakeConfirm(
+      Map<String, dynamic> params,
+      ) :super(name: 'stake_confirm_button_click', params: params);
+
+  /// Informs tracking services that stake was successful
+  FarmPageTrackingEvent.onStakeSuccess(
+      Map<String, dynamic> params,
+      ) :
+        super(name: 'stake_success', params: params);
+
+  /// Informs tracking services that claim rewards button pressed
+  FarmPageTrackingEvent.onPressedClaimRewards(
+      Map<String, dynamic> params,
+      ) :
+  super(name: 'claim_rewards_button_click', params: params);
+
+  /// Informs tracking services that UnStake button pressed
+  FarmPageTrackingEvent.onPressedUnStake(
+      Map<String, dynamic> params,
+      ) :
+        super(name: 'un_stake_approve_button_click', params: params);
+
+  /// Informs tracking services that UnStake confirm button was pressed
+  FarmPageTrackingEvent.onUnStakeConfirm(
+      Map<String, dynamic> params,
+      ) :
+  super(name: 'un_stake_confirm_button_click', params: params);
+
+  /// Informs tracking services that UnStake was successful
+  FarmPageTrackingEvent.onUnStakeSuccess(
+      Map<String, dynamic> params,
+      ) :
+  super(name: 'un_stake_success', params: params);
+
+}
