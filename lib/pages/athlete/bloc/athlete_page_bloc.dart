e77f6e1e2867461c94801c791a4220c1afe256b7
Pearlson Omesa <57809593+pearlson16@users.noreply.github.com>
2022-08-11 20:33:26 -0500

    Ax 441 frontend refactor nfl athlete data source (#508)
    * [Frontend] | Refactor NFLAthlete data source as well as Fetch NFL Players information from backend api
    
    * fix some flutter analysis issues
    
    * format fix
    
    * format fix
    
    * [Frontend] | Refactor NFLAthlete data source as well as Fetch NFL Players information from backend api
    
    * fix some flutter analysis issues
    
    * format fix
    
    * format fix
    
    * updated suppported nfl atheletes and fixed comments from pr
    
    * chore: removed unecessary comments from supported_nfl_athletes.dart
    
    Co-authored-by: Ryan Bharath <dbossman60@gmail.com>

--- lib/pages/athlete/bloc/athlete_page_bloc.dart
+++ lib/pages/athlete/bloc/athlete_page_bloc.dart
@@ -1,53 +1,118 @@
+import 'package:ax_dapp/pages/scout/models/athlete_scout_model.dart';
 import 'package:ax_dapp/repositories/mlb_repo.dart';
+import 'package:ax_dapp/repositories/nfl_repo.dart';
 import 'package:ax_dapp/util/bloc_status.dart';
 import 'package:ax_dapp/util/chart/extensions/graph_data.dart';
+import 'package:ax_dapp/util/supported_sports.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
 part 'athlete_page_event.dart';
+
 part 'athlete_page_state.dart';
 
 class AthletePageBloc extends Bloc<AthletePageEvent, AthletePageState> {
-  AthletePageBloc({required this.repo}) : super(AthletePageState.initial()) {
+  AthletePageBloc({
+    required this.mlbRepo,
+    required this.nflRepo,
+    required this.athlete,
+  }) : super(AthletePageState.initial()) {
     on<OnPageRefresh>(_mapPageRefreshEventToState);
     on<OnGraphRefresh>(_mapGraphRefreshEventToState);
   }
 
-  final MLBRepo repo;
+  final AthleteScoutModel athlete;
+
+  final MLBRepo mlbRepo;
+  final NFLRepo nflRepo;
 
   Future<void> _mapPageRefreshEventToState(
     OnPageRefresh event,
     Emitter<AthletePageState> emit,
   ) async {
-    try {
-      final playerId = event.playerId;
-      emit(state.copyWith(status: BlocStatus.loading));
-      final now = DateTime.now();
-      final startDate = DateTime(now.year, now.month - 1, now.day);
-      final formattedDate = DateFormat('yyyy-MM-dd').format(now);
-      final formattedStartDate = DateFormat('yyyy-MM-dd').format(startDate);
-      final until = formattedDate;
-      final from = formattedStartDate;
-      final stats = await repo.getPlayerStatsHistory(
-        playerId,
-        from,
-        until,
-      );
-      final graphStats = stats.statHistory
-          .map(
-            (stat) => GraphData(
-              DateFormat('yyy-MM-dd').parse(stat.timeStamp),
-              stat.price * 1000,
+    switch (athlete.sport) {
+      case SupportedSport.all:
+        break;
+
+      case SupportedSport.NFL:
+        try {
+          final playerId = event.playerId;
+          emit(state.copyWith(status: BlocStatus.loading));
+          final now = DateTime.now();
+          final startDate = DateTime(now.year, now.month - 10, now.day);
+          final formattedDate = DateFormat('yyyy-MM-dd').format(now);
+          final formattedStartDate = DateFormat('yyyy-MM-dd').format(startDate);
+          final until = formattedDate;
+          final from = formattedStartDate;
+          final stats = await nflRepo.getPlayerStatsHistory(
+            playerId,
+            from,
+            until,
+          );
+          final graphStats = stats.statHistory
+              .map(
+                (stat) => GraphData(
+                  DateFormat('yyy-MM-dd').parse(stat.timeStamp),
+                  stat.price * 10000,
+                ),
+              )
+              .toList();
+          final seenDates = <DateTime>{};
+          final distinctPoints = graphStats
+              .where((element) => seenDates.add(element.date))
+              .toList();
+          emit(
+            state.copyWith(
+              stats: distinctPoints,
+              status: BlocStatus.success,
             ),
-          )
-          .toList();
-      final seenDates = <DateTime>{};
-      final distinctPoints =
-          graphStats.where((element) => seenDates.add(element.date)).toList();
-      emit(state.copyWith(stats: distinctPoints, status: BlocStatus.success));
-    } catch (_) {
-      emit(state.copyWith(status: BlocStatus.error));
+          );
+        } catch (_) {
+          emit(state.copyWith(status: BlocStatus.error));
+        }
+        break;
+
+      case SupportedSport.MLB:
+        try {
+          final playerId = event.playerId;
+          emit(state.copyWith(status: BlocStatus.loading));
+          final now = DateTime.now();
+          final startDate = DateTime(now.year, now.month - 1, now.day);
+          final formattedDate = DateFormat('yyyy-MM-dd').format(now);
+          final formattedStartDate = DateFormat('yyyy-MM-dd').format(startDate);
+          final until = formattedDate;
+          final from = formattedStartDate;
+          final stats = await mlbRepo.getPlayerStatsHistory(
+            playerId,
+            from,
+            until,
+          );
+          final graphStats = stats.statHistory
+              .map(
+                (stat) => GraphData(
+                  DateFormat('yyy-MM-dd').parse(stat.timeStamp),
+                  stat.price * 1000,
+                ),
+              )
+              .toList();
+          final seenDates = <DateTime>{};
+          final distinctPoints = graphStats
+              .where((element) => seenDates.add(element.date))
+              .toList();
+          emit(
+            state.copyWith(
+              stats: distinctPoints,
+              status: BlocStatus.success,
+            ),
+          );
+        } catch (_) {
+          emit(state.copyWith(status: BlocStatus.error));
+        }
+        break;
+
+      case SupportedSport.NBA:
+        break;
     }
   }
 
