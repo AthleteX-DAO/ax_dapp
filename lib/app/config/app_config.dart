01c33aa229fa30496baeccee18ac91b35f946469
pmd <info@powermoves.io>
2022-08-25 10:33:51 -0400

    AX-853: add analytics env with dart-define

---
+++ lib/app/config/app_config.dart
@@ -0,0 +1,6 @@
+enum BuildConfig { staging, release }
+
+final buildConfig = BuildConfig.values.firstWhere(
+  (BuildConfig buildConfig) =>
+      buildConfig.name == const String.fromEnvironment('BUILD_TYPE',
+        defaultValue: 'staging',),);
