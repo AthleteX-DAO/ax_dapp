6942ec46387b9ddfbef45bface41a7b2f14449ba
Ryan Bharath <dbossman60@gmail.com>
2022-08-27 18:11:57 -0400

    chore: more print calls

--- lib/wallet/bloc/wallet_bloc.dart
+++ lib/wallet/bloc/wallet_bloc.dart
@@ -39,6 +39,7 @@ class WalletBloc extends Bloc<WalletEvent, WalletState> {
   ) async {
     try {
       final walletAddress = await _walletRepository.connectWallet();
+      print('wallet Address from the wallet bloc -> $walletAddress');
       emit(state.copyWith(walletAddress: walletAddress));
     } on WalletFailure catch (failure) {
       add(WalletFailed(failure));
