0d65a4022c2a95a22cc3fdb1afa0e6cc77a23a70
pmd <info@powermoves.io>
2022-09-03 00:23:54 -0400

    no-jira: update the incorrect nfl ids

--- lib/service/supported_athletes/supported_nfl_athletes.dart
+++ lib/service/supported_athletes/supported_nfl_athletes.dart
@@ -1,14 +1,14 @@
 class SupportedNFLAthletes {
-  static const int _jAllen = 20737;
+  static const int _jAllen = 19801;
   static const int _jHerbert = 21681;
   static const int _pMahomes = 18890;
   static const int _lJackson = 22049;
-  static const int _jTaylor = 21837;
+  static const int _jTaylor = 21682;
   static const int _dHenry = 17959;
   static const int _cMcCaffrey = 18877;
   static const int _aEkeler = 19562;
   static const int _cKupp = 18882;
-  static const int _jJefferson = 22555;
+  static const int _jJefferson = 21685;
   static const int _jChase = 22564;
   static const int _tKelce = 15048;
   static const int _mAndrews = 19803;
