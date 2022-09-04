e35af4b4b18591756119bcd8d3ae96449fc094c5
Pearlson Omesa <Pearlson.omesa@gmail.com>
2022-09-04 17:18:39 -0500

    dart analysis issues

--- packages/tokens_repository/lib/src/api/coin_gecko_api.g.dart
+++ packages/tokens_repository/lib/src/api/coin_gecko_api.g.dart
@@ -6,7 +6,8 @@ part of 'coin_gecko_api.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+// ignore_for_file: unnecessary_brace_in_string_interps,
+// no_leading_underscores_for_local_identifiers
 
 class _CoinGeckoAPI implements CoinGeckoAPI {
   _CoinGeckoAPI(this._dio, {this.baseUrl}) {
