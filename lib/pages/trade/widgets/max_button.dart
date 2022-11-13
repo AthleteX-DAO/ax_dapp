31dc94298a9eaa55c3e281325f0cf2eb840bf51b
Ryan Bharath <89420193+Friendlygeckos@users.noreply.github.com>
2022-11-12 22:26:18 -0500

    fix: text cutting off from pool and trade pages (#728)

--- lib/pages/trade/widgets/max_button.dart
+++ lib/pages/trade/widgets/max_button.dart
@@ -36,13 +36,17 @@ class MaxButton extends StatelessWidget {
             );
           _tokenFromInputController.text = tokenFromBalance;
         },
-        child: Text(
-          'MAX',
-          style: textStyle(
-            Colors.grey[400]!,
-            8,
-            isBold: false,
-            isUline: false,
+        child: FittedBox(
+          child: SizedBox(
+            child: Text(
+              'MAX',
+              style: textStyle(
+                Colors.grey[400]!,
+                8,
+                isBold: false,
+                isUline: false,
+              ),
+            ),
           ),
         ),
       ),
