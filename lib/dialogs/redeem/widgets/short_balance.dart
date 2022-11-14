138c1e551b3dbea366fa013a1b2f42adf4d0f550
Ryan Bharath <dbossman60@gmail.com>
2022-11-14 18:39:26 -0500

    chore: formatted the balance in redeem dialog to 6 decimal places

--- lib/dialogs/redeem/widgets/short_balance.dart
+++ lib/dialogs/redeem/widgets/short_balance.dart
@@ -1,4 +1,5 @@
 import 'package:ax_dapp/service/custom_styles.dart';
+import 'package:ax_dapp/util/helper.dart';
 import 'package:flutter/material.dart';
 
 class ShortBalance extends StatelessWidget {
@@ -11,13 +12,14 @@ class ShortBalance extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final balance = toDecimal(shortBalance, 6);
     return Container(
       margin: const EdgeInsets.only(right: 5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           Text(
-            'Balance: $shortBalance',
+            'Balance: $balance',
             style: textStyle(
               Colors.grey[600]!,
               15,
