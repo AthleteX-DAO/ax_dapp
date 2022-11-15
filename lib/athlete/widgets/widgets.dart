00c7527be47264481875a890bd06076623d49f86
Ryan Bharath <dbossman60@gmail.com>
2022-11-14 23:49:16 -0500

    refactor: separated the athlete page title into its own widget

--- lib/athlete/widgets/widgets.dart
+++ lib/athlete/widgets/widgets.dart
@@ -4,6 +4,7 @@ export 'athlete_page_long_graph.dart';
 export 'athlete_page_long_graph.dart';
 export 'athlete_page_short_graph.dart';
 export 'athlete_page_short_graph.dart';
+export 'athlete_page_title.dart';
 export 'athlete_page_tooltip.dart';
 export 'athlete_page_web_view.dart';
 export 'athlete_sell_approve_button.dart';
