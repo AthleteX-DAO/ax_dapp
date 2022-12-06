455b7533f6963efa4a11f4824dcfdc35d630a9aa
AirealConduit <107519047+AirealConduit@users.noreply.github.com>
2022-12-06 10:14:28 -0500

    chore: resolved merge conflicts due to branch being on a very old version of the app (#757)
    Co-authored-by: Ryan Bharath <dbossman60@gmail.com>

--- lib/dialogs/mint/widgets/you_receive.dart
+++ lib/dialogs/mint/widgets/you_receive.dart
@@ -13,7 +13,6 @@ class YouReceive extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const hgt = 450;
     return Flexible(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -37,64 +36,19 @@ class YouReceive extends StatelessWidget {
               Row(
                 children: [
                   SizedBox(
-                    width: hgt * 0.2,
-                    child: Text(
-                      longReceive.toStringAsFixed(6),
-                      style: textStyle(
-                        Colors.white,
-                        15,
-                        isBold: false,
-                        isUline: false,
+                    width: 315,
+                    child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text(
+                        '${longReceive.toStringAsFixed(6)} Long APTs + ${shortReceive.toStringAsFixed(6)} Short APTs',
+                        style: textStyle(
+                          Colors.white,
+                          15,
+                          isBold: false,
+                          isUline: false,
+                        ),
+                        maxLines: 1,
                       ),
-                      maxLines: 1,
-                    ),
-                  ),
-                  SizedBox(
-                    child: Text(
-                      'Long APTs',
-                      style: textStyle(
-                        Colors.white,
-                        15,
-                        isBold: false,
-                        isUline: false,
-                      ),
-                      maxLines: 1,
-                    ),
-                  ),
-                  SizedBox(
-                    child: Text(
-                      ' + ',
-                      style: textStyle(
-                        Colors.white,
-                        15,
-                        isBold: false,
-                        isUline: false,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: hgt * 0.2,
-                    child: Text(
-                      shortReceive.toStringAsFixed(6),
-                      style: textStyle(
-                        Colors.white,
-                        15,
-                        isBold: false,
-                        isUline: false,
-                      ),
-                      maxLines: 1,
-                    ),
-                  ),
-                  SizedBox(
-                    child: Text(
-                      'Short APTs',
-                      style: textStyle(
-                        Colors.white,
-                        15,
-                        isBold: false,
-                        isUline: false,
-                      ),
-                      maxLines: 1,
                     ),
                   ),
                 ],
