fc3d82ce54891e9ebce4b15dc7f3059518122368
Ryan Bharath <dbossman60@gmail.com>
2023-01-05 11:18:03 -0500

    chore: moved javascript call files and updated package URI

--- lib/wallet/repository/web3_auth_repository.dart
+++ lib/wallet/repository/web3_auth_repository.dart
@@ -1,11 +1,11 @@
+import 'package:ax_dapp/wallet/javascript_calls/web3_auth.dart';
+import 'package:ax_dapp/wallet/models/models.dart';
 import 'package:flutter/foundation.dart';
-import 'package:ax_dapp/web/libraries/web3auth.dart';
-
-// TODO(kevin): create a User class that will store user information
 
 class Web3AuthRepository {
   Web3AuthRepository({required Web3Auth web3auth}) : _web3Auth = web3Auth;
   final Web3Auth _web3Auth;
+
   /// [connect] will allow the user to connect with a provider using web3Auth
   /// It will return a [User] if the connection is successful
   /// Otherwise, it will return an empty [User] if the connection fails
@@ -20,9 +20,9 @@ class Web3AuthRepository {
     }
   }
 
-  /// [logout] will allow the user to signout from their web2 profile
+  /// [logout] will sign the [User] out of the profile
   void logout() {
-    _web3Auth.logOut();
+    _web3Auth.logout();
   }
 
   /// [getUserInfo] will retrieve the user information when the user logs in 
