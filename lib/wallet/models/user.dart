fe67e1989c3652b700d11de37c236b85244beb3a
Ryan Bharath <dbossman60@gmail.com>
2023-01-05 11:08:18 -0500

    chore: fixed User class

--- lib/wallet/models/user.dart
+++ lib/wallet/models/user.dart
@@ -1,8 +1,6 @@
 import 'package:shared/shared.dart';
-import 'package:flutter/foundation.dart';
 
 /// {@template user}
-
 class User extends Equatable {
 
     const User({
@@ -16,53 +14,53 @@ class User extends Equatable {
         this.idToken,
         this.oAuthIdToken,
         this.oAuthAccessToken,
-    })
+    });
 
     /// `User` email
-    final String email;
+    final String? email;
 
     /// `User` name
-    final String name;
+    final String? name;
     
     /// `User` profileImage
-    final String profileImage;
+    final String? profileImage;
 
     /// `User` aggregateVerifier
-    final String aggregateVerifier;
+    final String? aggregateVerifier;
     
     /// `User` verifier
-    final String verifier;
+    final String? verifier;
 
     /// `User` typeOfLogin
-    final String typeOfLogin;
+    final String? typeOfLogin;
 
     /// `User` dappShare
-    final String dappShare;
+    final String? dappShare;
 
     /// `User` idToken
-    final String idToken;
+    final String? idToken;
 
     /// `User` oAuthIdToken
-    final String oAuthIdToken;
+    final String? oAuthIdToken;
 
     /// `User` oAuthAccessToken
-    final String oAuthAccessToken;
+    final String? oAuthAccessToken;
 
     @override
     List<Object?> get props =>
     [email, name, profileImage, aggregateVerifier, verifier, typeOfLogin, dappShare, idToken, oAuthIdToken, oAuthAccessToken];
 
     User copyWith({
-        String email,
-        String name,
-        String profileImage,
-        String aggregateVerifier,
-        String verifier,
-        String typeOfLogin,
-        String dappShare,
-        String idToken,
-        String oAuthIdToken,
-        String oAuthAccessToken
+        String? email,
+        String? name,
+        String? profileImage,
+        String? aggregateVerifier,
+        String? verifier,
+        String? typeOfLogin,
+        String? dappShare,
+        String? idToken,
+        String? oAuthIdToken,
+        String? oAuthAccessToken,
     }) {
         return User(
             email: email ?? this.email,
@@ -74,24 +72,7 @@ class User extends Equatable {
             dappShare: dappShare ?? this.dappShare,
             idToken: idToken ?? this.idToken,
             oAuthIdToken: oAuthIdToken ?? this.oAuthIdToken,
-            oAuthAccessToken: oAuthAccessToken ?? this.oAuthAccessToken
+            oAuthAccessToken: oAuthAccessToken ?? this.oAuthAccessToken,
         );
     }
 }
-
-
-
-// Reference: 
-// {
-//   "email": "john@gmail.com",
-//   "name": "John Dash",
-//   "profileImage": "https://lh3.googleusercontent.com/a/Ajjjsdsmdjmnm...",
-//   "aggregateVerifier": "tkey-google-lrc",
-//   "verifier": "torus",
-//   "verifierId": "john@gmail.com",
-//   "typeOfLogin": "google",
-//   "dappShare": "<24 words seed phrase>", // will be sent only incase of custom verifiers
-//   "idToken": "<jwtToken issued by Web3Auth>",
-//   "oAuthIdToken": "<jwtToken issued by OAuth Provider>", // will be sent only incase of custom verifiers
-//   "oAuthAccessToken": "<accessToken issued by OAuth Provider>" // will be sent only incase of custom verifiers
-// }
