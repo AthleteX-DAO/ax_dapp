021403faa87c2b6f10d314eabdb829baaac467e7
Ryan Bharath <dbossman60@gmail.com>
2023-01-17 18:09:42 -0500

    chore: added getters and setters to the transaction receipt class

--- lib/wallet/javascript_calls/transaction_receipt.dart
+++ lib/wallet/javascript_calls/transaction_receipt.dart
@@ -3,18 +3,40 @@ library userinfo;
 import 'package:js/js.dart';
 
 @JS()
-class TransactionReceipt {
+abstract class TransactionReceipt {
   external factory TransactionReceipt({
     bool status,
     String transactionHash,
-    int transactionIndex,
+    num transactionIndex,
     String blocHash,
-    int blockNumber,
+    num blockNumber,
     String from,
     String to,
     String? contractAddress,
-    double cumulativeGasUsed,
-    double gasUsed,
-    double effectiveGasPrice,
+    num cumulativeGasUsed,
+    num gasUsed,
+    num effectiveGasPrice,
   });
+  external bool get status;
+  external set status(bool value);
+  external String get transactionHash;
+  external set transactionHash(String value);
+  external num get transactionIndex;
+  external set transactionIndex(num value);
+  external String get blockHash;
+  external set blockHash(String value);
+  external num get blockNumber;
+  external set blockNumber(num value);
+  external String get from;
+  external set from(String value);
+  external String get to;
+  external set to(String value);
+  external String get contractAddress;
+  external set contractAddress(String value);
+  external num get cumulativeGasUsed;
+  external set cumulativeGasUsed(num value);
+  external num get gasUsed;
+  external set gasUsed(num value);
+  external num get effectiveGasPrice;
+  external set effectiveGasPrice(num value);
 }
