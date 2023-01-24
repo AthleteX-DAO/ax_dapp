eb1228eac4c3ca5d4cc7c17db2637d98316bfbab
Ryan Bharath <dbossman60@gmail.com>
2023-01-24 01:53:32 -0500

    chore: added more methods that are defined from the web3 library

--- web/libraries/Magic.js
+++ web/libraries/Magic.js
@@ -1,4 +1,4 @@
-var Magic = (function() {
+var Magic = (function () {
     function Magic(apiKey, chainId, rpcUrl, locale) {
         this._magic = new Magic({
             apiKey,
@@ -12,21 +12,50 @@ var Magic = (function() {
         console.log("constructor", _this.magic);
     }
 
-    Magic.prototype.showWallet = async function() {
+    Magic.prototype.showWallet = async function () {
         console.log("show wallet")
         await _this.magic.connect.showWallet();
     }
 
-    Magic.prototype.disconnect = async function() {
+    Magic.prototype.disconnect = async function () {
         console.log("disconnect");
         await _this.magic.connect.disconnect();
     }
 
-    Magic.prototype.getWalletInfo = async function() {
+    Magic.prototype.getWalletInfo = async function () {
         console.log("get wallet info");
         const walletInfo = await _this.magic.connect.getWalletInfo();
         console.log(walletInfo);
         return walletInfo;
+    }
+
+    Magic.prototype.requestUserInfo = async function () {
+        console.log("request user information");
+        const email = await _this.magic.connect.requestUserInfo();
+        return email;
+    }
+
+    Magic.prototype.getIdToken = async function () {
+        console.log("get id token");
+        const idToken = await _this.magic.user.getIdToken();
+        return idToken;
+    }
+
+    Magic.prototype.getMetadata = async function () {
+        console.log("metadata -> user information");
+        const { issuer, email, phoneNumber, publicAddress } = await _this.magic.user.getMetadata();
+        return { issuer, email, phoneNumber, publicAddress };
+    }
+
+    Magic.prototype.isLoggedIn = async function () {
+        console.log("is the user logged in to the app");
+        const isLoggedIn = await _this.magic.user.isLoggedIn();
+        return isLoggedIn;
+    }
+
+    Magic.prototype.logOut = async function () {
+        console.log("logging the user out");
+        await _this.magic.user.logOut();
     }
 
     return Magic;
