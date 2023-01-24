9f1c088274f888733311e50b70b988e320952cc9
Ryan Bharath <dbossman60@gmail.com>
2023-01-24 02:46:42 -0500

    chore: starting to work on a magic repository that will handle the methods for the Magic SDK

---
+++ lib/wallet/repository/magic_repository.dart
@@ -0,0 +1,52 @@
+import 'package:ax_dapp/wallet/javascript_calls/magic.dart';
+import 'package:ax_dapp/wallet/javascript_calls/web3_rpc.dart';
+
+class MagicRepository {
+  MagicRepository({required Magic magic, required Web3RPC web3rpc})
+      : _magic = magic,
+        _web3rpc = web3rpc;
+  final Magic _magic;
+  final Web3RPC _web3rpc;
+
+  // TODO(anyone): complete the connect method
+  // Future<dynamic> connect() async {
+  //   await _web3rpc.getAccounts();
+  // }
+
+  Future<void> showWallet() async {
+    await _magic.showWallet();
+  }
+
+  Future<void> disconnect() async {
+    await _magic.disconnect();
+  }
+
+  Future<dynamic> getWalletInfo() async {
+    final walletInfo = await _magic.getWalletInfo();
+    return walletInfo;
+  }
+
+  Future<dynamic> requestUserInfo() async {
+    final email = await _magic.requestUserInfo();
+    return email;
+  }
+
+  Future<dynamic> getIdToken() async {
+    final idToken = await _magic.getIdToken();
+    return idToken;
+  }
+
+  //TODO(anyone): return a class(?) that has the issuer, email, phoneNumber, publicAddress
+  Future<dynamic> getMetadata() async {
+    await _magic.getMetadata();
+  }
+
+  Future<dynamic> isLoggedIn() async {
+    final isLoggedIn = await _magic.isLoggedIn();
+    return isLoggedIn;
+  }
+
+  Future<void> logOut() async {
+    await _magic.logOut();
+  }
+}
