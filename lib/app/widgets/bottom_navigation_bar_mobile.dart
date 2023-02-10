7a48735dd0c6c350e7884e8687000b77f0be66e7
faguirregarc2015 <89745045+faguirregarc2015@users.noreply.github.com>
2023-02-10 06:41:18 -0500

    Feat/ax 1149 update navigation bar add league tab (#795)
    * chore: fixing changes
    
    * fix: replaced image asset with an icon that shows the trophy
    
    ---------
    
    Co-authored-by: Ryan Bharath <dbossman60@gmail.com>

--- lib/app/widgets/bottom_navigation_bar_mobile.dart
+++ lib/app/widgets/bottom_navigation_bar_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 
 class BottomNavigationBarMobile extends StatelessWidget {
@@ -68,6 +69,14 @@ class BottomNavigationBarMobile extends StatelessWidget {
           ),
           label: 'Farm',
         ),
+        BottomNavigationBarItem(
+          icon: FaIcon(
+            FontAwesomeIcons.trophy,
+            size: 24,
+            color: iconColor(4),
+          ),
+          label: 'League',
+        ),
       ],
       currentIndex: selectedIndex,
       selectedItemColor: Colors.white,
@@ -85,6 +94,9 @@ class BottomNavigationBarMobile extends StatelessWidget {
             break;
           case 3:
             context.goNamed('farm');
+            break;
+          case 4:
+            context.goNamed('league');
             break;
         }
       },
