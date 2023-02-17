1a3b86a606476b3ee43b3d362b540f9cc872acc6
Ryan Bharath <89420193+Friendlygeckos@users.noreply.github.com>
2023-02-17 11:36:16 -0500

    Revert "fix: reset the date to previous state (#803)"
    This reverts commit 35fcf9bf442b073928111bbd07f68b633385ca09.

--- lib/scout/usecases/get_scout_athletes_data_use_case.dart
+++ lib/scout/usecases/get_scout_athletes_data_use_case.dart
@@ -37,8 +37,7 @@ class GetScoutAthletesDataUseCase {
     List<SportAthlete> players,
   ) async {
     final ids = players.map((player) => player.id).toList();
-    final now = DateTime.now();
-    final startDate = DateTime(now.year, now.month, now.day - 2);
+    final startDate = DateTime(2022, 06, 02);
     final formattedStartDate = DateFormat('yyyy-MM-dd').format(startDate);
     try {
       final histories = await repo.getPlayersPriceHistory(
