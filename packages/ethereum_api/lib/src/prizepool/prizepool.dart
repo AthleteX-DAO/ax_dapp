05434598131d1221e356558936ca22cf858f930f
Kevin Kamto <kamtokevin@gmail.com>
2023-04-17 14:09:21 -0400

    docs: added relevant commenting

--- packages/ethereum_api/lib/src/prizepool/prizepool.dart
+++ packages/ethereum_api/lib/src/prizepool/prizepool.dart
@@ -1,13 +1,16 @@
 /// A Placeholder PrizePool.sol ABI
 ///
 ///
-
 class PrizePool {
+  /// The Prize Pool is where each league's funds are stored
   PrizePool();
 
+  /// Join a newly created league
   void joinLeague() {}
 
+  /// Withdraw from a league before it starts
   void withdrawBeforeLeagueStarts() {}
 
+  /// Pay out the prize to the winner of the league
   void distributePrize() {}
 }
