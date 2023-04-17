10635cc01da733f79be2bee8e3ac182aba64c5f9
Kevin Kamto <kamtokevin@gmail.com>
2023-04-17 14:06:29 -0400

    chore(prizepool.dart): created data later

--- lib/league/repository/prizepool_repository.dart
+++ lib/league/repository/prizepool_repository.dart
@@ -1,5 +1,6 @@
 import 'package:ax_dapp/service/controller/controller.dart';
 import 'package:web3dart/web3dart.dart';
+import 'package:ethereum_api/prizepool_api.dart';
 import 'package:get/get.dart';
 
 class PrizePoolRepository {
@@ -9,10 +10,18 @@ class PrizePoolRepository {
   String adminAddress = '';
 
   set admin(String address) => adminAddress;
-
+  late PrizePool prizePool;
   String get admin => adminAddress;
 
-  Future<void> joinPrizePool() async {}
-  Future<void> withdrawBeforePoolStarts() async {}
-  Future<void> distributePrize(String winnerAddress) async {}
+  Future<void> joinPrizePool() async {
+    prizePool.joinLeague();
+  }
+
+  Future<void> withdrawBeforePoolStarts() async {
+    prizePool.withdrawBeforeLeagueStarts();
+  }
+
+  Future<void> distributePrize(String winnerAddress) async {
+    prizePool.distributePrize();
+  }
 }
--- packages/ethereum_api/lib/src/prizepool/prizepool.dart
+++ packages/ethereum_api/lib/src/prizepool/prizepool.dart
@@ -1,1 +1,13 @@
-export '';
+/// A Placeholder PrizePool.sol ABI
+///
+///
+
+class PrizePool {
+  PrizePool();
+
+  void joinLeague() {}
+
+  void withdrawBeforeLeagueStarts() {}
+
+  void distributePrize() {}
+}
