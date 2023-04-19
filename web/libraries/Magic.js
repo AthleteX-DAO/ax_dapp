82798025a37791f5495ec4c662ffa1c62469340b
Kevin Kamto <kamtokevin@gmail.com>
2023-04-19 11:48:52 -0400

    deps(magicSDK): updated to new library

--- web/libraries/Magic.js
+++ web/libraries/Magic.js
@@ -1,29 +1,17 @@
 var MagicSDK = (function () {
-    function MagicSDK(apiKey, chainId, rpcUrl, locale, extension) {
-        this._magicSDK = new Magic('pk_live_A0EFC48FF2C1D624', {
-            // network: {
-            //     chainId, chainId,
-            //     rpcUrl: rpcUrl,
-            // },
-            network: "goerli",
-            locale: "en_US",
-            extensions: [new MagicConnectExtension()],
-        });
+    function MagicSDK(apiKey) {
+        this._magicSDK = new Magic('pk_live_A0EFC48FF2C1D624');
         this._web3 = new Web3(this._magicSDK.rpcProvider);
         
         /// Places the magic ethereum client onto the window object
         window.magicEthereum = this._web3.eth;
-        this.eth = this._web3.eth;
         console.log(Web3);
-        console.log(this._magicSDK);
-        console.log(this._magicSDK.rpcProvider);
-        console.log(`Comparing web3 obj: \n ${Web3} to the window.magicEthereum obj: ${window.magicEthereum}`)
     }
 
     MagicSDK.prototype.connect = async function () {
         console.log("Connecting to Magic");      
-        console.log(this._web3);
         const address = (await this._web3.eth.getAccounts())[0];
+        console.log(this._web3);
         console.log("This is the address " + address);
         console.log(typeof address);
         return address;
@@ -56,6 +44,6 @@ var MagicSDK = (function () {
         const credentials = await this._web3.eth.requestAccount();
         return credentials;
     }
-
+q
     return MagicSDK;
 })();
