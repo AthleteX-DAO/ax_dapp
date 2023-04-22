9e4ba2501af81617a50ac4ef014a007b3cd8d490
Kevin Kamto <kamtokevin@gmail.com>
2023-04-22 15:35:17 -0400

    fix: sylized details

--- lib/predict/widgets/show_details_widget.dart
+++ lib/predict/widgets/show_details_widget.dart
@@ -2,7 +2,7 @@ import 'package:ax_dapp/service/custom_styles.dart';
 import 'package:ax_dapp/util/colors.dart';
 import 'package:flutter/material.dart';
 
-class ShowDetailsWidget extends StatefulWidget {
+class ShowDetailsWidget extends StatelessWidget {
   ShowDetailsWidget({
     super.key,
     required this.promptDetails,
@@ -10,36 +10,38 @@ class ShowDetailsWidget extends StatefulWidget {
 
   final String promptDetails;
 
-  @override
-  State<ShowDetailsWidget> createState() => _ShowDetailsWidget();
-}
-
-class _ShowDetailsWidget extends State<ShowDetailsWidget> {
-  final String promptDetails = 'details';
   List<bool> isSelected = [false];
   @override
   Widget build(BuildContext context) {
-    return ToggleButtons(
-      isSelected: isSelected,
-      onPressed: (int index) {
-        setState(() {
-          {
-            isSelected[index] = !isSelected[index];
-          }
-        });
-      },
-      fillColor: primaryOrangeColor,
-      children: [
-        Text(
-          'Show Details >',
-          style: textStyle(
-            primaryWhiteColor,
-            20,
-            isBold: false,
-            isUline: false,
-          ),
-        ),
-      ],
+    final _width = MediaQuery.of(context).size.width;
+    var wid = _width * 0.4;
+    if (_width < 1160) wid = _width * 0.95;
+
+    return SizedBox(
+      width: wid,
+      height: 400,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Text(
+                  promptDetails,
+                  overflow: TextOverflow.fade,
+                  style: textStyle(
+                    Colors.white,
+                    20,
+                    isBold: false,
+                    isUline: false,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
