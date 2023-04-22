2d3f6ec1280a45ad8593e28adf1cc047f1d6d06a
Kevin Kamto <kamtokevin@gmail.com>
2023-04-22 12:40:22 -0400

    chore: begun refactoring graph side

--- lib/predict/widgets/graph_side.dart
+++ lib/predict/widgets/graph_side.dart
@@ -6,23 +6,19 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class GraphSide extends StatelessWidget {
-  const GraphSide(
-      {super.key,
-      required this.predictionModel,
-      required this.containerHeight,
-      required this.containerWidth});
+  const GraphSide({
+    super.key,
+    required this.predictionModel,
+  });
 
   final PredictionModel predictionModel;
-  // final List<GraphData> chartStats;
-  final double containerHeight;
-  final double containerWidth;
+  // final List<GraphData> chartStats;  final double containerWidth;
 
   @override
   Widget build(BuildContext context) {
     final _width = MediaQuery.of(context).size.width;
     final _height = MediaQuery.of(context).size.height;
-    var wid = _width * 0.4;
-    if (_width < 1160) wid = containerWidth;
+    final wid = _width * 0.4;
     const indexUnselectedStackBackgroundColor = Colors.transparent;
     final _zoomPanBehavior = ZoomPanBehavior(
       enableMouseWheelZooming: true,
