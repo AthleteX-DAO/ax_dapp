4131799774e8d08c97c558c5601cd15d3846bb45
Kevin Kamto <kamtokevin@gmail.com>
2023-04-26 21:07:58 -0400

    fix: setup of bloc architecture

--- lib/prediction/bloc/prediction_page_state.dart
+++ lib/prediction/bloc/prediction_page_state.dart
@@ -3,21 +3,25 @@ part of 'prediction_page_bloc.dart';
 class PredictionPageState extends Equatable {
   const PredictionPageState({
     this.status = BlocStatus.initial,
+    this.predictionModel = PredictionModel.empty,
     this.predictionAddress = kEmptyAddress,
   });
 
   final BlocStatus status;
+  final PredictionModel predictionModel;
   final String predictionAddress;
 
   PredictionPageState copyWith({
     String? predictionAddress,
+    PredictionModel? predictionModel,
     required BlocStatus status,
   }) {
     return PredictionPageState(
       predictionAddress: predictionAddress ?? this.predictionAddress,
+      predictionModel: predictionModel ?? this.predictionModel,
     );
   }
 
   @override
-  List<Object?> get props => [predictionAddress, status];
+  List<Object?> get props => [predictionAddress, status, predictionModel];
 }
