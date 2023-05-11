58689c70e3517f5f8a134768b4ef3ab56c474535
Kevin Kamto <kamtokevin@gmail.com>
2023-05-11 07:15:58 -0400

    chore: cleanup unnecessary code

--- lib/prediction/widgets/desktop_prediction.dart
+++ lib/prediction/widgets/desktop_prediction.dart
@@ -1,6 +1,7 @@
 import 'package:ax_dapp/athlete/athlete.dart';
 import 'package:ax_dapp/predict/models/prediction_model.dart';
 import 'package:ax_dapp/predict/widgets/Probability.dart';
+import 'package:ax_dapp/prediction/bloc/prediction_page_bloc.dart';
 import 'package:ax_dapp/prediction/widgets/prompt_details.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -20,22 +21,17 @@ class DesktopPrediction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _width = MediaQuery.of(context).size.width;
-    final _prompt = predictionModel.prompt;
     return SizedBox(
       height: 70,
       child: OutlinedButton(
         onPressed: () {
-          final param_prompt = predictionModel.prompt;
-          final param_details = predictionModel.details;
           context.goNamed(
             'prediction',
-            params: {'id': param_prompt},
+            params: {'id': predictionModel.prompt},
           );
         },
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-          // TODO: Update Buttons to match Figma
           children: <Widget>[
             // prompt, probability
             Row(
