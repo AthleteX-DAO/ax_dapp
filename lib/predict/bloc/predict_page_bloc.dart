06bba7d339c47b82a83506ed9e831a7f52cd60f1
Kevin Kamto <kamtokevin@gmail.com>
2023-05-11 07:20:03 -0400

    feat: new questions!

--- lib/predict/bloc/predict_page_bloc.dart
+++ lib/predict/bloc/predict_page_bloc.dart
@@ -18,46 +18,42 @@ class PredictPageBloc extends Bloc<PredictPageEvent, PredictPageState> {
     on<WatchAppDataChangesStarted>(_onWatchAppDataChangesStarted);
     on<LoadPredictionsEvent>(_onLoadPredictions);
     add(const WatchAppDataChangesStarted());
-    add(const LoadPredictionsEvent());
   }
 
   final WalletRepository _walletRepository;
   final StreamAppDataChangesUseCase _streamAppDataChanges;
   final List<PredictionModel> questions = [
     const PredictionModel(
-      prompt: 'Houston Astros vs Tampa Bay Rays',
+      prompt: 'Celtics vs 76ers: Game 6',
       details:
           'This market will resolve to “Red Sox” if Boston Red Sox win the game against Baltimore Orioles on April 26, 2023. If the Baltimore Orioles win the game the market will resolve to “Orioles”. The resolution source for this market will be MLB or a consensus of credible reporting will also suffice. If the fight is canceled, postponed, or ends in a draw, Yes and No will resolve to 50/50.',
-      address: '',
+      address: '0x990898a024ecE7E1CA39FbDed9244001d4ec0c6d',
+      yesTokenAddress: '0x9e193Dce9F58262EE8f9090E3346Fc24F28e5fEF',
+      noTokenAddress: '0x208AD891f67E229306a3202bf5d77d7712aA0b8d',
     ),
     const PredictionModel(
-      prompt: 'Washington Nationals vs New York Mets',
+      prompt: 'Nuggets vs Suns: Game 6',
       details:
-          'This market will resolve to “Knicks” if the New York Knicks win the game against the Cleveland Cavaliers on April 26, 2023. If the Cleveland Cavaliers win the game the market will resolve to “Cavaliers”. The resolution source for this market will be NBA or a consensus of credible reporting will also suffice. If the fight is canceled, postponed, or ends in a draw, Yes and No will resolve to 50/50.',
-      address: '',
+          'This market will resolve to “Nuggets” if Denver Nuggets win the game against Phoenix Suns on May 11, 2023. If the Phoenix Suns win the game the market will resolve to “Suns”. The resolution source for this market will be NBA or a consensus of credible reporting will also suffice. If the game is canceled, postponed, or ends in a draw, Yes and No will resolve to 50/50.',
+      address: '0x98E944F4e4205afEb936f56e39Ae469De8DFbE3d',
+      yesTokenAddress: '0x300aE756362A1f60528e4FB4729da5CD9b6CF3a1',
+      noTokenAddress: '0x36E3554522f6F3a94f1616bC9A0c11E7E6a8d229',
     ),
     const PredictionModel(
-      prompt: 'New York Knicks vs Cleveland Cavaliers',
+      prompt: 'Devils vs Hurricanes: Game 5',
       details:
-          'This market will resolve to “Yes” if the Gervonta Davis is knocked down at least once. Otherwise, this market will resolve to “No”. A “No” resolution would mean Garcia does not knockdown Davis during the fight. The resolution source for this market will be WBA or a consensus of credible reporting will also suffice. If the fight is canceled or postponed, Yes and No will resolve to 50/50.',
-      address: '',
+          'This market will resolve to “Hurricanes” if Carolina Hurricanes win the game against the New Jersey Devils on May 11, 2023. If the New Jersey Devils win the game the market will resolve to “Devils”. The resolution source for this market will be NHL or a consensus of credible reporting will also suffice. If the game is canceled, postponed, or ends in a draw, Yes and No will resolve to 50/50.',
+      address: '0x0FA810C2E8C3A50938afcD057eDA4364b19372F9',
+      yesTokenAddress: '0x59B395B954F6A175646823453C8C5a7eDf8e0887',
+      noTokenAddress: '0x839F330F636f2a98Ae6bEE5aC8bCCA03D2572314',
     ),
     const PredictionModel(
-      prompt: 'Los Angeles Lakers vs Memphis Grizzlies',
+      prompt: 'Kraken vs Stars: Game 5',
       details:
-          'This market will resolve to “Davis” if Gervonta Davis is knocked down more than Garcia. This market will resolve to “Garcia” if Ryan Garcia is knocked down more than Davis. The resolution source for this market will be WBA or a consensus of credible reporting will also suffice. If Davis and Garcia are knocked down equally, Yes and No will resolve to 50/50. If the fight is canceled or postponed, Yes and No will resolve to 50/50.',
-      address: '',
-    ),
-    const PredictionModel(
-      prompt: 'Miami Heat vs Milwaukee Bucks',
-      details:
-          'This market will resolve to “Yes” if the winner of the fight cries during the in-ring post-fight interview. If the winner of the fight does not shed a tear in the ring during the post-fight interview, this market will resolve to “No”. Post-fight press conferences outside the ring do not apply to this market. The resolution source for this market will be post-fight interview video or a consensus of credible reporting will also suffice. If the fight is canceled or postponed, Yes and No will resolve to 50/50.',
-      address: '',
-    ),
-    const PredictionModel(
-      prompt: 'Golden State Warriors vs Sacramento Kings',
-      details: '',
-      address: '',
+          'This market will resolve to “Kraken” if Seattle Kraken win the game against the Dallas Stars on May 11, 2023. If the Dallas Stars win the game the market will resolve to “Stars”. The resolution source for this market will be NHL or a consensus of credible reporting will also suffice. If the game is canceled, postponed, or ends in a draw, Yes and No will resolve to 50/50.',
+      address: '0xC45085e3e9F3fE86BFBc10D0eEBa28a1359b5A54',
+      yesTokenAddress: '0xa3529A89f34519B37EDC84d6e106eB718A4CE02e',
+      noTokenAddress: '0xD356CFF6F58Fd037C8cAFD02f3179fB95A08655c',
     ),
   ];
 
@@ -73,6 +69,8 @@ class PredictPageBloc extends Bloc<PredictPageEvent, PredictPageState> {
             status: BlocStatus.loading,
           ),
         );
+
+        final appConfig = appData.appConfig;
       },
     );
   }
@@ -82,8 +80,17 @@ class PredictPageBloc extends Bloc<PredictPageEvent, PredictPageState> {
     Emitter<PredictPageState> emit,
   ) async {
     emit(
-      state.copyWith(predictions: questions, status: BlocStatus.initial),
+      state.copyWith(predictions: [], status: BlocStatus.loading),
     );
+
+    if (questions.isNotEmpty) {
+      emit(
+        state.copyWith(
+          predictions: questions,
+          status: BlocStatus.success,
+        ),
+      );
+    }
   }
 
   Future<void> _onYesButtonTapped() async {
