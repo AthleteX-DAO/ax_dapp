727049f3290eaa02196f6db8961ce2a22f94ec80
Kevin Kamto <kamtokevin@gmail.com>
2023-06-13 14:24:12 -0400

    fix: magicSDK updates

--- packages/ethereum_api/lib/src/wallet/magic_api_client/javascript_calls/magic.dart
+++ packages/ethereum_api/lib/src/wallet/magic_api_client/javascript_calls/magic.dart
@@ -4,6 +4,8 @@ library magic_sdk;
 import 'package:js/js.dart';
 
 @JS()
+
+/// {template} MagicSDK:
 class MagicSDK {
   external MagicSDK(
     String apiKey,
@@ -17,5 +19,8 @@ class MagicSDK {
   external Future<dynamic> getWalletInfo();
   external Future<dynamic> requestUserInfo();
   external Future<dynamic> getGasPrice();
+  external Future<dynamic> getChainId();
+  external Future<dynamic> getDecimals(String tokenAddress);
+  external Future<dynamic> addToken(String tokenAddress, String tokenUrl);
   external Future<dynamic> requestAccount();
 }
