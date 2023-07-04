caacecd2caa6dbb9b19f12723fca74c9a9555b94
Ryan Bharath <dbossman60@gmail.com>
2023-07-04 12:51:34 -0400

    refactor: organized imports for mint button

--- lib/prediction/widgets/buttons/mint/mint_button.dart
+++ lib/prediction/widgets/buttons/mint/mint_button.dart
@@ -1,18 +1,12 @@
+import 'package:ax_dapp/predict/models/prediction_model.dart';
 import 'package:ax_dapp/prediction/widgets/buttons/mint/bloc/mint_button_bloc.dart';
 import 'package:ax_dapp/service/controller/predictions/event_market_repository.dart';
-import 'package:ax_dapp/wallet/bloc/wallet_bloc.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ax_dapp/util/athlete_page_format_helper.dart';
 import 'package:ax_dapp/util/colors.dart';
-import 'package:ax_dapp/util/util.dart';
-import 'package:ax_dapp/predict/models/prediction_model.dart';
-import 'package:ax_dapp/prediction/bloc/prediction_page_bloc.dart';
-import 'package:ax_dapp/prediction/view/prediction_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:use_cases/stream_app_data_changes_use_case.dart';
 import 'package:wallet_repository/wallet_repository.dart';
-import '';
 
 class GenericMintButton extends StatelessWidget {
   const GenericMintButton({
