40ae413d46911fd21db0df0a35d2a2f63cab5d27
Ryan Bharath <dbossman60@gmail.com>
2023-08-16 13:07:53 -0400

    chore: removed unecessary tests

--- test/test_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Testing unit tests', () {
-    const actual = 'This should execute successfully';
-    const matcher = 'This should execute successfully';
-    expect(actual, matcher);
-  });
-}
