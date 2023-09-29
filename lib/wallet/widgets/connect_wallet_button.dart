f81945507e6af8a628ed37af023877dea5b5e521
Ryan Bharath <dbossman60@gmail.com>
2023-09-29 11:03:08 -0400

    fix: 'connect' now shows wallet address when the wallet is connected

--- lib/wallet/widgets/connect_wallet_button.dart
+++ lib/wallet/widgets/connect_wallet_button.dart
@@ -1,16 +1,34 @@
+import 'package:ax_dapp/wallet/bloc/wallet_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ConnectWalletButton extends StatelessWidget {
   const ConnectWalletButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isWalletConnected =
+        context.select((WalletBloc bloc) => bloc.state.isWalletConnected);
+    final walletAddress =
+        context.select((WalletBloc bloc) => bloc.state.walletAddress);
     final _width = MediaQuery.of(context).size.width;
     var width = 180.0;
-    var text = 'Connect ';
+    var text = 'Connect';
+    var formattedWalletAddress = '';
+    if (walletAddress.isNotEmpty) {
+      final walletAddressPrefix = walletAddress.substring(0, 7);
+      final walletAddressSuffix = walletAddress.substring(
+        walletAddress.length - 5,
+        walletAddress.length,
+      );
+      formattedWalletAddress = '$walletAddressPrefix...$walletAddressSuffix';
+    }
     if (_width < 565) {
       width = 110;
-      text = 'Connect';
+    }
+
+    if (isWalletConnected) {
+      text = formattedWalletAddress;
     }
 
     return Container(
@@ -25,13 +43,15 @@ class ConnectWalletButton extends StatelessWidget {
         onPressed: () {
           Scaffold.of(context).openEndDrawer();
         },
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.amber[400],
-            fontFamily: 'OpenSans',
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
+        child: FittedBox(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.amber[400],
+              fontFamily: 'OpenSans',
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
           ),
         ),
       ),
