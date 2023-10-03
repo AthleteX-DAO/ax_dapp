99a124a92f03175bc7775ffa2319bce54a949493
Kevin Kamto <kamtokevin@gmail.com>
2023-10-03 08:59:38 -0400

    chore: separated out mobile markets filter

--- lib/markets/widgets/markets_filter.dart
+++ lib/markets/widgets/markets_filter.dart
@@ -6,21 +6,155 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tokens_repository/tokens_repository.dart';
 
 class MarketsFilter extends StatelessWidget {
-  const MarketsFilter({super.key, required BoxConstraints boxConstraints})
-      : constraints = boxConstraints;
+  const MarketsFilter({
+    super.key,
+    required BoxConstraints boxConstraints,
+    required MarketsPageState pageState,
+  })  : constraints = boxConstraints,
+        state = pageState;
 
   final BoxConstraints constraints;
+  final MarketsPageState state;
+
+  Widget mobile() {
+    return BlocConsumer<MarketsPageBloc, MarketsPageState>(
+      builder: (context, state) {
+        const sportFilterTxSz = 14.0;
+        var bloc = context.read<MarketsPageBloc>();
+        var input = '';
+        final myController = TextEditingController(text: input);
+
+        var _selectedMarkets = state.selectedMarket;
+        return Row(
+          children: [
+            Text(
+              'Live Markets',
+              style: textStyle(
+                Colors.white,
+                18,
+                isBold: false,
+                isUline: false,
+              ),
+            ),
+            Text(
+              '|',
+              style: textStyle(
+                Colors.white,
+                18,
+                isBold: false,
+                isUline: false,
+              ),
+            ),
+            // TextButton(
+            //   onPressed: () {
+            //     _selectedMarkets = SupportedMarkets.all;
+
+            //     context.read<MarketsPageBloc>().add(
+            //           const SelectedMarketsChanged(
+            //             selectedMarkets: SupportedMarkets.all,
+            //           ),
+            //         );
+            //   },
+            //   child: Text(
+            //     'ALL',
+            //     style: textSwapState(
+            //       condition: _selectedMarkets == SupportedMarkets.all,
+            //       tabNotSelected: textStyle(
+            //         Colors.white,
+            //         sportFilterTxSz,
+            //         isBold: false,
+            //         isUline: false,
+            //       ),
+            //       tabSelected: textStyle(
+            //         Colors.amber[400]!,
+            //         sportFilterTxSz,
+            //         isBold: false,
+            //         isUline: true,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            TextButton(
+              onPressed: () {
+                myController.clear();
+                _selectedMarkets = SupportedMarkets.Athlete;
+                bloc
+                  ..add(
+                    const SelectedMarketsChanged(
+                      selectedMarkets: SupportedMarkets.Athlete,
+                    ),
+                  )
+                  ..add(FetchScoutInfoRequested());
+              },
+              child: Text(
+                'Athlete',
+                style: textSwapState(
+                  condition: _selectedMarkets == SupportedMarkets.Athlete,
+                  tabNotSelected: textStyle(
+                    Colors.white,
+                    sportFilterTxSz,
+                    isBold: false,
+                    isUline: false,
+                  ),
+                  tabSelected: textStyle(
+                    Colors.amber[400]!,
+                    sportFilterTxSz,
+                    isBold: false,
+                    isUline: true,
+                  ),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                myController.clear();
+                _selectedMarkets = SupportedMarkets.Sports;
+                bloc
+                  ..add(
+                    const SelectedMarketsChanged(
+                      selectedMarkets: SupportedMarkets.Sports,
+                    ),
+                  )
+                  ..add(
+                    FetchSportsMarketsRequested(),
+                  );
+              },
+              child: Text(
+                'Sports',
+                style: textSwapState(
+                  condition: _selectedMarkets == SupportedMarkets.Sports,
+                  tabNotSelected: textStyle(
+                    Colors.white,
+                    sportFilterTxSz,
+                    isBold: false,
+                    isUline: false,
+                  ),
+                  tabSelected: textStyle(
+                    Colors.amber[400]!,
+                    sportFilterTxSz,
+                    isBold: false,
+                    isUline: true,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+      listener: (context, state) {},
+    );
+  }
 
   @override
   Container build(BuildContext context) {
     var input = '';
     final myController = TextEditingController(text: input);
     final bloc = context.read<MarketsPageBloc>();
-    var _selectedMarkets = SupportedMarkets.all;
+    var _selectedMarkets = state.selectedMarket;
     var _selectedSport = SupportedSport.all;
     const sportFilterTxSz = 14.0;
 
-    // APT Title & Sport Filter
+    // Sport Filter
     return Container(
       margin: const EdgeInsets.only(
         left: 20,
@@ -50,46 +184,48 @@ class MarketsFilter extends StatelessWidget {
               isUline: false,
             ),
           ),
-          TextButton(
-            onPressed: () {
-              myController.clear();
-              _selectedMarkets = SupportedMarkets.all;
-              _selectedSport = SupportedSport.all;
+          // TextButton(
+          //   onPressed: () {
+          //     myController.clear();
+          //     _selectedMarkets = SupportedMarkets.all;
+          //     _selectedSport = SupportedSport.all;
 
-              context.read<MarketsPageBloc>().add(
-                    const SelectedMarketsChanged(
-                      selectedMarkets: SupportedMarkets.all,
-                    ),
-                  );
-            },
-            child: Text(
-              'ALL',
-              style: textSwapState(
-                condition: _selectedMarkets == SupportedMarkets.all,
-                tabNotSelected: textStyle(
-                  Colors.white,
-                  sportFilterTxSz,
-                  isBold: false,
-                  isUline: false,
-                ),
-                tabSelected: textStyle(
-                  Colors.amber[400]!,
-                  sportFilterTxSz,
-                  isBold: false,
-                  isUline: true,
-                ),
-              ),
-            ),
-          ),
+          //     context.read<MarketsPageBloc>().add(
+          //           const SelectedMarketsChanged(
+          //             selectedMarkets: SupportedMarkets.all,
+          //           ),
+          //         );
+          //   },
+          //   child: Text(
+          //     'ALL',
+          //     style: textSwapState(
+          //       condition: _selectedMarkets == SupportedMarkets.all,
+          //       tabNotSelected: textStyle(
+          //         Colors.white,
+          //         sportFilterTxSz,
+          //         isBold: false,
+          //         isUline: false,
+          //       ),
+          //       tabSelected: textStyle(
+          //         Colors.amber[400]!,
+          //         sportFilterTxSz,
+          //         isBold: false,
+          //         isUline: true,
+          //       ),
+          //     ),
+          //   ),
+          // ),
           TextButton(
             onPressed: () {
               myController.clear();
               _selectedMarkets = SupportedMarkets.Athlete;
-              bloc.add(
-                const SelectedMarketsChanged(
-                  selectedMarkets: SupportedMarkets.Athlete,
-                ),
-              );
+              bloc
+                ..add(
+                  const SelectedMarketsChanged(
+                    selectedMarkets: SupportedMarkets.Athlete,
+                  ),
+                )
+                ..add(FetchScoutInfoRequested());
             },
             child: Text(
               'Athlete',
