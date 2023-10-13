a0844a541ac2d40c2b1c2e4b497f0336981a3bf1
Kevin Kamto <kamtokevin@gmail.com>
2023-10-13 08:28:19 -0600

    chore: added a lot more widgets

--- lib/wallet/widgets/widgets.dart
+++ lib/wallet/widgets/widgets.dart
@@ -1,3 +1,12 @@
+export 'ax_information_widgets.dart';
+export 'connect_wallet_button.dart';
 export 'connect_wallet_button.dart';
 export 'dialogs/dialogs.dart';
+export 'login_text.dart';
+export 'sign_up_button.dart';
+export 'wallet_account.dart';
+export 'wallet_ax.dart';
+export 'wallet_chain.dart';
+export 'wallet_gas.dart';
 export 'wallet_profile.dart';
+export 'wallet_view.dart';
