ae220622b80f6f315c49504b48b1e8d2bf4d2df1
Kevin Kamto <kamtokevin@gmail.com>
2023-10-17 09:27:38 -0600

    fix: request wallet when logging in

--- lib/wallet/widgets/login_text.dart
+++ lib/wallet/widgets/login_text.dart
@@ -30,6 +30,7 @@ class Login extends StatelessWidget {
             recognizer: TapGestureRecognizer()
               ..onTap = () {
                 context.read<WalletBloc>().add(const LoginViewRequested());
+                context.read<WalletBloc>().add(const ConnectWalletRequested());
               },
           ),
         ],
