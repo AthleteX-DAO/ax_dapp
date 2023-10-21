2458a160c392590ac12cd23213ef79df30db4cec
Kevin Kamto <kamtokevin@gmail.com>
2023-10-20 19:20:18 -0600

    feat: basic signup page

--- lib/wallet/view/signup.dart
+++ lib/wallet/view/signup.dart
@@ -1,16 +1,81 @@
+import 'package:ax_dapp/util/colors.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:ax_dapp/wallet/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class SignUpView extends StatelessWidget {
-  const SignUpView({
+  SignUpView({
     super.key,
   });
 
+  final kTextFieldDecoration = const InputDecoration(
+    hintText: 'Enter a value',
+    hintStyle: TextStyle(color: Colors.grey),
+    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: const EdgeInsets.all(10),
-      child: Text('SignupView!'),
+    final _auth = FirebaseAuth.instance;
+    String email;
+    String password;
+    var showSpinner = false;
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                email = value;
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintText: 'Enter your email',
+              ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                password = value;
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintText: 'Enter your Password',
+              ),
+            ),
+            const SizedBox(
+              height: 24.0,
+            ),
+            ElevatedButton(
+              child: Text('Sign Up'),
+              onPressed: () async {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
