f1ab2f7383bc613685f4ded1fd47826c48a22741
Kevin Kamto <kamtokevin@gmail.com>
2023-10-22 09:34:14 -0600

    feat: login page button refactor

--- lib/wallet/view/login.dart
+++ lib/wallet/view/login.dart
@@ -1,81 +1,120 @@
+import 'package:ax_dapp/util/colors.dart';
+import 'package:ax_dapp/wallet/bloc/wallet_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({
+  LoginView({
     super.key,
   });
 
+  final kTextFieldDecoration = InputDecoration(
+    hintText: '',
+    hintStyle: const TextStyle(color: Colors.white),
+    fillColor: secondaryOrangeColor,
+    contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+    border: const OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(32)),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: primaryOrangeColor),
+      borderRadius: const BorderRadius.all(Radius.circular(32)),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: primaryOrangeColor, width: 2),
+      borderRadius: const BorderRadius.all(Radius.circular(32)),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    final nameController = TextEditingController();
-    final passwordController = TextEditingController();
-
-    return SingleChildScrollView(
-      child: ListView(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(10),
-            child: const Text(
-              'Sign in',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'User Name',
+    String email;
+    String password;
+    var showSpinner = false;
+    final _auth = FirebaseAuth.instance;
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                email = value;
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintText: 'Enter your email',
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-            child: TextField(
+            const SizedBox(
+              height: 12,
+            ),
+            TextField(
               obscureText: true,
-              controller: passwordController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                password = value;
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintText: 'Enter your password.',
               ),
             ),
-          ),
-          TextButton(
-            onPressed: () {
-              //forgot password screen
-            },
-            child: const Text(
-              'Forgot Password',
+            const SizedBox(
+              height: 35,
             ),
-          ),
-          Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: ElevatedButton(
-                child: const Text('Login'),
-                onPressed: () {
-                  print(nameController.text);
-                  print(passwordController.text);
-                },
-              )),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('Does not have account?'),
-              TextButton(
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 20),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  primaryOrangeColor,
                 ),
-                onPressed: () {
-                  //signup screen
-                },
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: const BorderSide(color: Colors.white),
+                  ),
+                ),
+                minimumSize: MaterialStateProperty.all<Size>(
+                  Size(width / 4, height * 0.09),
+                ),
+                maximumSize: MaterialStateProperty.all<Size>(
+                  Size(width / 2, height * 0.10),
+                ),
               ),
-            ],
-          ),
-        ],
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              onPressed: () async {
+                context.read<WalletBloc>().add(const ProfileViewRequested());
+
+                // TODO: Sign up with email & password
+                // try {
+                //   final user = await _auth.signInWithEmailAndPassword(
+                //       email: email, password: password);
+                // } catch (e) {
+                //   print(e);
+                // }
+
+                showSpinner = false;
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
