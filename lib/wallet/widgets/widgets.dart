c78cd0bca0712ed4cca314bceb1cb6c1ff648a35
Kevin Kamto <kamtokevin@gmail.com>
2023-10-22 09:49:49 -0600

    fix: removed dead widgets

--- lib/wallet/widgets/widgets.dart
+++ lib/wallet/widgets/widgets.dart
@@ -1,6 +1,5 @@
 export 'ax_information_widgets.dart';
 export 'connect_wallet_button.dart';
-export 'connect_wallet_button.dart';
 export 'dialogs/dialogs.dart';
 export 'login_text.dart';
 export 'sign_up_button.dart';
@@ -8,5 +7,4 @@ export 'wallet_account.dart';
 export 'wallet_ax.dart';
 export 'wallet_chain.dart';
 export 'wallet_gas.dart';
-export 'wallet_profile.dart';
 export 'wallet_view.dart';
