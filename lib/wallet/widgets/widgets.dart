ead54bc64e51872de6cf9950cbf181112b12ed46
Kevin Kamto <kamtokevin@gmail.com>
2023-10-24 13:13:59 -0600

    chore: removed deleted file

--- lib/wallet/widgets/widgets.dart
+++ lib/wallet/widgets/widgets.dart
@@ -4,7 +4,6 @@ export 'dialogs/dialogs.dart';
 export 'login_text.dart';
 export 'sign_up_button.dart';
 export 'wallet_account.dart';
-export 'wallet_ax.dart';
 export 'wallet_chain.dart';
 export 'wallet_gas.dart';
 export 'wallet_view.dart';
