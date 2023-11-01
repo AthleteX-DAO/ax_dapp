181bfb09d5d1a6fadb526131ddcdc8618c8b6067
Kevin Kamto <kamtokevin@gmail.com>
2023-10-31 23:05:17 -0600

    chore: setup of the Kado API

---
+++ lib/wallet/repository/kado_api_repository.dart
@@ -0,0 +1,9 @@
+import 'package:wallet_repository/wallet_repository.dart';
+
+class KadoAPIRepository {
+  String baseDataUrl = 'https://api.kado.money/';
+
+  Future<void> buy() async {}
+
+  Future<void> sell() async {}
+}
