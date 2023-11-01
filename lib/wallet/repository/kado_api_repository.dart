747ca09cdc22a78d7da10a7b44d43424f80df2b6
Kevin Kamto <kamtokevin@gmail.com>
2023-11-01 09:32:55 -0600

    fix: renamed repo

--- lib/wallet/repository/kado_api_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:wallet_repository/wallet_repository.dart';
-
-class KadoAPIRepository {
-  String baseDataUrl = 'https://api.kado.money/';
-
-  Future<void> buy() async {}
-
-  Future<void> sell() async {}
-}
