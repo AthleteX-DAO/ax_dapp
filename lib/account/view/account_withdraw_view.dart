205640bec101599309598a5ec95bec6e65cedeb7
Kevin Kamto <kamtokevin@gmail.com>
2023-11-08 12:49:30 -0700

    chore: WIP account withdraw view

--- lib/account/view/account_withdraw_view.dart
+++ lib/account/view/account_withdraw_view.dart
@@ -8,41 +8,58 @@ class AccountWithdrawView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const edge = 40;
+
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
         return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            IconButton(
-              alignment: Alignment.centerLeft,
-              icon: const Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-                size: 30,
-              ),
-              onPressed: () => context.read<AccountBloc>().add(
-                    const AccountDetailsViewRequested(),
-                  ),
-            ),
             SizedBox(
+              height: 45,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    alignment: Alignment.centerLeft,
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                    onPressed: () => context.read<AccountBloc>().add(
+                          const AccountDetailsViewRequested(),
+                        ),
+                  ),
+                  Text(
+                    'Select a Token',
+                    style: textStyle(
+                      Colors.white,
+                      16,
+                      isBold: true,
+                      isUline: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 65,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Row(
-                    children: [
-                      Text(
-                        'Your Withdraw Details',
-                        style: textStyle(
-                          Colors.grey[600]!,
-                          13,
-                          isBold: false,
-                          isUline: false,
-                        ),
-                      )
-                    ],
-                  )
+                    children: [],
+                  ),
                 ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              width: constraints.maxWidth - edge,
+              child: const Divider(
+                color: Colors.grey,
               ),
             ),
           ],
