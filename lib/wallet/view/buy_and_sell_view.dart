1cbc1f5852e849a34d93d7459391b63dc633f6db
Kevin Kamto <kamtokevin@gmail.com>
2023-11-07 19:11:14 -0700

    feat: basic implementation of widget

---
+++ lib/wallet/view/buy_and_sell_view.dart
@@ -0,0 +1,47 @@
+import 'dart:html';
+import 'dart:ui' as ui;
+import 'package:ax_dapp/service/custom_styles.dart';
+import 'package:ax_dapp/util/util.dart';
+import 'package:easy_web_view/easy_web_view.dart';
+import 'package:flutter/material.dart';
+
+class BuyAndSell extends StatefulWidget {
+  const BuyAndSell({super.key});
+
+  @override
+  State<BuyAndSell> createState() => _BuyAndSellState();
+}
+
+class _BuyAndSellState extends State<BuyAndSell> {
+  late Widget kadoMoney;
+  final _iframeElement = IFrameElement();
+
+  @override
+  void initState() {
+    super.initState();
+    _iframeElement
+      ..height = '620'
+      ..width = '480'
+      ..src =
+          'https://app.kado.money/?apiKey=137cd949-ab0f-429f-93b8-187ef3a93862';
+    _iframeElement.style.border = 'none';
+
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+      'iframeElement',
+      (int viewId) => _iframeElement,
+    );
+
+    kadoMoney = HtmlElementView(
+      key: UniqueKey(),
+      viewType: 'iframeElement',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: kadoMoney,
+    );
+  }
+}
