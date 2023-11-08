f2b7236376d91717ffd74b97395deecbcaffa7e3
Kevin Kamto <kamtokevin@gmail.com>
2023-11-08 12:50:00 -0700

    feat: view token img & ticker

--- lib/account/widgets/account_assets_card.dart
+++ lib/account/widgets/account_assets_card.dart
@@ -1,24 +1,78 @@
+import 'package:ax_dapp/service/custom_styles.dart';
+import 'package:ax_dapp/util/util.dart';
 import 'package:flutter/material.dart';
 import 'package:tokens_repository/tokens_repository.dart';
 
-class WalletAssetCard extends StatelessWidget {
-  const WalletAssetCard({
+class AccountAssetCard extends StatelessWidget {
+  const AccountAssetCard({
     super.key,
-    required this.tokenAsset,
+    required this.token,
   });
 
-  final Token tokenAsset;
+  final Token token;
 
   @override
   Widget build(BuildContext context) {
+    final _width = MediaQuery.of(context).size.width;
+
     return SizedBox(
       height: 40,
       child: OutlinedButton(
         onPressed: () {},
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Text('Your Asset: ${tokenAsset.name}'),
+            Container(
+              height: 30,
+              width: 60,
+              alignment: Alignment.centerLeft,
+              child: Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    scale: 0.5,
+                    image: tokenImage(token),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 45,
+              // ticker/name column "AX/AthleteX"
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    width: (_width < 350.0) ? 110 : 125,
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      token.ticker,
+                      style: textStyle(
+                        Colors.white,
+                        14,
+                        isBold: true,
+                        isUline: false,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: (_width < 350.0) ? 110 : 125,
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      token.name,
+                      style: textStyle(
+                        Colors.grey[100]!,
+                        9,
+                        isBold: false,
+                        isUline: false,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
