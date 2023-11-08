35c48a1e03a874e98bb032359c5976a8b0d444ca
Kevin Kamto <kamtokevin@gmail.com>
2023-11-07 22:43:30 -0700

    refactor: renamed files for ease of understanding

---
+++ lib/wallet/widgets/wallet_top_bar.dart
@@ -0,0 +1,48 @@
+import 'package:ax_dapp/service/tracking/tracking_cubit.dart';
+import 'package:ax_dapp/util/helper.dart';
+import 'package:ax_dapp/wallet/wallet.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class WalletTopBar extends StatelessWidget {
+  const WalletTopBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<WalletBloc, WalletState>(
+      listenWhen: (previous, current) =>
+          previous.axData == AxData.empty &&
+          current.axData != AxData.empty &&
+          current.isWalletConnected,
+      listener: (context, state) {
+        final walletAddress = context.read<WalletBloc>().state.walletAddress;
+        context.read<TrackingCubit>().onConnectWalletSuccessful(
+              publicAddress: walletAddress,
+              axUnits: '"${toDecimal(state.axData.balance!, 6)} AX"',
+              walletType: 'MetaMask',
+            );
+      },
+      child: BlocConsumer<WalletBloc, WalletState>(
+        listenWhen: (previous, current) =>
+            previous.walletStatus != current.walletStatus ||
+            previous.failure != current.failure,
+        listener: (_, state) {
+          if (state.isWalletUnsupported || state.isWalletUnavailable) {
+            showDialog<void>(
+              context: context,
+              builder: (_) => const WrongNetworkDialog(),
+            );
+          }
+        },
+        buildWhen: (previous, current) =>
+            previous.walletStatus != current.walletStatus,
+        builder: (_, state) {
+          if (state.isWalletConnected) {
+            return const WalletTopBarDetails();
+          }
+          return const ConnectWalletButton();
+        },
+      ),
+    );
+  }
+}
