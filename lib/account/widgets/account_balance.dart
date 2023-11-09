6f01792b719475db922c9239f86d3d18443762a4
Ryan Bharath <dbossman60@gmail.com>
2023-11-08 20:09:20 -0500

    fix: show usdc balance when connecting wallet (The balance next to the disconnect button)

--- lib/account/widgets/account_balance.dart
+++ lib/account/widgets/account_balance.dart
@@ -1,17 +1,24 @@
 import 'package:ax_dapp/service/custom_styles.dart';
 import 'package:ax_dapp/wallet/bloc/wallet_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 
 class WalletBalance extends StatelessWidget {
   const WalletBalance({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var walletBalance =
-        context.select((WalletBloc bloc) => bloc.state.walletBalance);
+    const chain = EthereumChain.none;
+    var walletBalance = 0.0;
+    final isConnected =
+        context.select((WalletBloc bloc) => bloc.state.isWalletConnected);
+    final currentChain = context.select((WalletBloc bloc) => bloc.state.chain);
+    if (isConnected && chain != currentChain) {
+      context.read<WalletBloc>().add(const FetchWalletBalanceRequested());
+      walletBalance =
+          context.select((WalletBloc bloc) => bloc.state.walletBalance);
+    }
+
     return TextButton(
       onPressed: () {
         context.read<WalletBloc>().add(const FetchWalletBalanceRequested());
@@ -23,19 +30,14 @@ class WalletBalance extends StatelessWidget {
             Icons.attach_money,
             color: Colors.white,
           ),
-          BlocSelector<WalletBloc, WalletState, double>(
-            selector: (state) => state.walletBalance,
-            builder: (_, walletBalance) {
-              return Text(
-                '$walletBalance',
-                style: textStyle(
-                  Colors.white,
-                  20,
-                  isBold: false,
-                  isUline: false,
-                ),
-              );
-            },
+          Text(
+            '$walletBalance',
+            style: textStyle(
+              Colors.white,
+              20,
+              isBold: false,
+              isUline: false,
+            ),
           ),
         ],
       ),
