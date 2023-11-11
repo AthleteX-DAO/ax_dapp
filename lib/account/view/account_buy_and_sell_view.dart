732b478bf2dc7b78dce355f2673a54aca0e0826d
Ryan Bharath <dbossman60@gmail.com>
2023-11-11 18:45:55 -0500

    chore: linter fixes on account buy and sell view

--- lib/account/view/account_buy_and_sell_view.dart
+++ lib/account/view/account_buy_and_sell_view.dart
@@ -46,7 +46,7 @@ class _AccountBuyAndSellState extends State<AccountBuyAndSell> {
   Widget build(BuildContext context) {
     final _height = MediaQuery.of(context).size.height;
     const edge = 40.0;
-    var wid = 400.0;
+    const wid = 400.0;
 
     const edge2 = 60.0;
     return LayoutBuilder(
@@ -56,7 +56,6 @@ class _AccountBuyAndSellState extends State<AccountBuyAndSell> {
           mainAxisSize: MainAxisSize.min,
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 IconButton(
                   alignment: Alignment.centerLeft,
