16dc4b5a826204bbda9c3d10f7cbca7e319d3e04
Ryan Bharath <dbossman60@gmail.com>
2023-11-12 00:57:24 -0500

    chore: removed redundant function

--- lib/util/helper.dart
+++ lib/util/helper.dart
@@ -6,10 +6,6 @@ BigInt days(int x) {
   return BigInt.from(60 * 60 * 24 * x);
 }
 
-Future<DateTime> now() async {
-  return DateTime.now();
-}
-
 String toDecimal(double val, int decimal) {
   final num = val * (pow(10, decimal));
   final converted = num.truncate();
