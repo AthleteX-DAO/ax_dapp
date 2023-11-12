718cf5be18f39f4c73a83e3b3e1219e11edc15e7
Ryan Bharath <dbossman60@gmail.com>
2023-11-12 01:00:16 -0500

    chore: removed redundant code

--- lib/util/helper.dart
+++ lib/util/helper.dart
@@ -1,11 +1,5 @@
 import 'dart:math';
 
-const kDecimals = 18;
-
-BigInt days(int x) {
-  return BigInt.from(60 * 60 * 24 * x);
-}
-
 String toDecimal(double val, int decimal) {
   final num = val * (pow(10, decimal));
   final converted = num.truncate();
