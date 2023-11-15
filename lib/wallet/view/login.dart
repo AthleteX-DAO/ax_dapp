3035aa9c835f7cb3b212440ea856046e8c796b2b
Ryan Bharath <89420193+rbharath99@users.noreply.github.com>
2023-11-15 18:12:18 -0500

    fix: warning toast shows if there is any error with login or signup (#907)

--- lib/wallet/view/login.dart
+++ lib/wallet/view/login.dart
@@ -1,6 +1,5 @@
 import 'package:ax_dapp/util/util.dart';
 import 'package:ax_dapp/wallet/bloc/wallet_bloc.dart';
-import 'package:ax_dapp/wallet/models/models.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -22,151 +21,155 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    return BlocListener<WalletBloc, WalletState>(
-      listener: (context, state) {
+    return BlocBuilder<WalletBloc, WalletState>(
+      buildWhen: (previous, current) => previous != current,
+      builder: (context, state) {
+        final bloc = context.read<WalletBloc>();
+        final errorMessage = state.errorMessage;
+        final walletViewStatus = state.walletViewStatus;
         if (state.hasFailure) {
-          context.showWarningToast(
-            title: 'Error',
-            description: state.errorMessage ?? 'Authentication Error',
-          );
-          context
-              .read<WalletBloc>()
-              .add(const AuthFailed(walletViewStatus: WalletViewStatus.login));
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            context.showWarningToast(
+              title: 'Error',
+              description: errorMessage ?? 'Authentication Error',
+            );
+          });
+          bloc.add(AuthFailed(walletViewStatus: walletViewStatus));
         }
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            IconButton(
-              alignment: Alignment.centerLeft,
-              icon: const Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-                size: 30,
-              ),
-              onPressed: () => context.read<WalletBloc>().add(
-                    const LoginSignUpViewRequested(),
-                  ),
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              controller: emailController,
-              textAlign: TextAlign.center,
-              onChanged: (value) =>
-                  context.read<WalletBloc>().add(EmailChanged(email: value)),
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
-                labelText: 'Email',
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            TextField(
-              obscureText: true,
-              controller: passwordController,
-              textAlign: TextAlign.center,
-              onChanged: (value) => context
-                  .read<WalletBloc>()
-                  .add(PassWordChanged(password: value)),
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password.',
-                labelText: 'Password',
-              ),
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  primaryOrangeColor.withOpacity(0.15),
-                ),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    side: const BorderSide(color: Colors.white),
-                  ),
-                ),
-                minimumSize: MaterialStateProperty.all<Size>(
-                  Size(width / 4, height * 0.09),
-                ),
-                maximumSize: MaterialStateProperty.all<Size>(
-                  Size(width / 2, height * 0.10),
-                ),
-              ),
-              child: const Text(
-                'Login',
-                style: TextStyle(
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              IconButton(
+                alignment: Alignment.centerLeft,
+                icon: const Icon(
+                  Icons.arrow_back,
                   color: Colors.white,
-                  fontSize: 40,
-                  fontFamily: 'OpenSans',
-                  fontWeight: FontWeight.w400,
+                  size: 30,
+                ),
+                onPressed: () => context.read<WalletBloc>().add(
+                      const LoginSignUpViewRequested(),
+                    ),
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                controller: emailController,
+                textAlign: TextAlign.center,
+                onChanged: (value) =>
+                    context.read<WalletBloc>().add(EmailChanged(email: value)),
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                  labelText: 'Email',
                 ),
               ),
-              onPressed: () async {
-                context.read<WalletBloc>().add(
-                      const ProfileViewRequestedFromLogin(),
-                    );
-              },
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(
-                    text: 'Connect',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        context
-                            .read<WalletBloc>()
-                            .add(const ConnectWalletRequested());
-                      },
-                  ),
-                  TextSpan(
-                    text: ', with Metamask or another external wallet',
-                    style: TextStyle(
-                      color: Colors.amber[400],
-                      fontSize: 14,
-                      fontFamily: 'OpenSans',
-                    ),
-                  ),
-                ],
+              const SizedBox(
+                height: 12,
               ),
-            ),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(
-                    text: 'Forgot Password?',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        context
-                            .read<WalletBloc>()
-                            .add(const ResetPasswordViewRequested());
-                      },
-                  ),
-                ],
+              TextField(
+                obscureText: true,
+                controller: passwordController,
+                textAlign: TextAlign.center,
+                onChanged: (value) => context
+                    .read<WalletBloc>()
+                    .add(PassWordChanged(password: value)),
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password.',
+                  labelText: 'Password',
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
+              const SizedBox(
+                height: 35,
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    primaryOrangeColor.withOpacity(0.15),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      side: const BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  minimumSize: MaterialStateProperty.all<Size>(
+                    Size(width / 4, height * 0.09),
+                  ),
+                  maximumSize: MaterialStateProperty.all<Size>(
+                    Size(width / 2, height * 0.10),
+                  ),
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontFamily: 'OpenSans',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                onPressed: () async {
+                  context.read<WalletBloc>().add(
+                        const ProfileViewRequestedFromLogin(),
+                      );
+                },
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Connect',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          context
+                              .read<WalletBloc>()
+                              .add(const ConnectWalletRequested());
+                        },
+                    ),
+                    TextSpan(
+                      text: ' with Metamask or another external wallet',
+                      style: TextStyle(
+                        color: Colors.amber[400],
+                        fontSize: 14,
+                        fontFamily: 'OpenSans',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Forgot Password?',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          context
+                              .read<WalletBloc>()
+                              .add(const ResetPasswordViewRequested());
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 
