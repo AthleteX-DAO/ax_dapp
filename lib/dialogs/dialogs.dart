a09e61c3b349a04f00d01c7e5957df1d3ee61037
Kevin Kamto <kamtokevin@gmail.com>
2023-11-17 10:02:27 -0700

    chore(dilaogs): created an inclusive exports file

---
+++ lib/dialogs/dialogs.dart
@@ -0,0 +1,42 @@
+/// Global exports for the dialogs folder
+///
+/// This is intended to simplify imports/exports for dialogs
+
+/// Buy Dialogs
+export 'buy/bloc/buy_dialog_bloc.dart';
+export 'buy/buy_dialog.dart';
+export 'buy/models/models.dart';
+export 'buy/widgets/widgets.dart';
+
+/// Mint Dialogs
+export 'mint/bloc/mint_dialog_bloc.dart' hide WatchAptPairStarted;
+export 'mint/mint_dialog.dart';
+export 'mint/widgets/widgets.dart' hide Balance;
+
+/// Promo
+export 'promo/connected_wallet_promo_dialog.dart';
+
+/// Redeem Dialogs
+export 'redeem/bloc/redeem_dialog_bloc.dart' hide WatchAptPairStarted;
+export 'redeem/redeem_dialog.dart';
+export 'redeem/widgets/widgets.dart';
+
+/// Sell Dialogs
+export 'sell/bloc/sell_dialog_bloc.dart'
+    hide
+        noTokenInfoMessage,
+        BuyDialogStateX,
+        AptTypeSelectionChanged,
+        WatchAptPairStarted,
+        InSufficientFailure,
+        exceptionMessage,
+        UpdateSwapController;
+export 'sell/sell_dialog.dart' hide LongAptButton, ShortAptButton;
+export 'sell/widgets/widgets.dart'
+    hide
+        Balance,
+        MarketPriceImpact,
+        MinimumReceived,
+        Price,
+        TotalFees,
+        SlippageTolerance;
