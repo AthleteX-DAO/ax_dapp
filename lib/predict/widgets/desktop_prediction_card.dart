c906e73b5fcb01a524dbf4f8a520ff2117bde999
Kevin Kamto <kamtokevin@gmail.com>
2023-11-20 18:54:23 -0700

    feat: completed desktop prediction card

--- lib/predict/widgets/desktop_prediction_card.dart
+++ lib/predict/widgets/desktop_prediction_card.dart
@@ -1,7 +1,8 @@
 import 'package:ax_dapp/predict/predict.dart';
 import 'package:ax_dapp/predict/widgets/24h_volume.dart';
 import 'package:ax_dapp/predict/widgets/price.dart';
-import 'package:ax_dapp/prediction/widgets/buy_prediction_button.dart';
+import 'package:ax_dapp/predict/widgets/widget_factories/prediction_details_widget.dart';
+import 'package:ax_dapp/prediction/widgets/prediction_buy_button.dart';
 import 'package:ax_dapp/service/custom_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -33,16 +34,20 @@ class DesktopPredictionCard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             // Prediction Prompt
-            PredictionPrompt(predictionModel: predictionModel),
+            PredictionDetailsWidget(predictionModel)
+                .predictionDetailsCardsForWeb(_width),
 
+            // Yes Price
             MarketPrice(predictionModel: predictionModel, isYesToken: true),
+            // No Price
+            MarketPrice(predictionModel: predictionModel, isYesToken: false),
 
             // price
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 PredictionBuyButton(
-                  model: predictionModel,
+                  predictionModel: predictionModel,
                 ),
                 if (_width >= 1090) ...[
                   const SizedBox(
