1698f51c93ee788e6519ba5491385d1ef4571272
Kevin Kamto <kamtokevin@gmail.com>
2023-11-23 15:03:26 -0700

    feat: setup of prediction page web view

--- lib/prediction/view/prediction_page.dart
+++ lib/prediction/view/prediction_page.dart
@@ -1,10 +1,13 @@
 import 'package:ax_dapp/predict/models/prediction_model.dart';
 import 'package:ax_dapp/prediction/bloc/prediction_page_bloc.dart';
-import 'package:ax_dapp/prediction/widgets/graph_side.dart';
-import 'package:ax_dapp/prediction/widgets/prompt_details.dart';
+import 'package:ax_dapp/prediction/repository/prediction_address_repository.dart';
+import 'package:ax_dapp/prediction/view/prediction_page_web_view.dart';
+import 'package:ax_dapp/service/controller/predictions/event_market_repository.dart';
 import 'package:ax_dapp/util/bloc_status.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:use_cases/stream_app_data_changes_use_case.dart';
+import 'package:wallet_repository/wallet_repository.dart';
 
 class PredictionPage extends StatelessWidget {
   const PredictionPage({
@@ -16,81 +19,38 @@ class PredictionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _width = MediaQuery.of(context).size.width;
-    final _height = MediaQuery.of(context).size.height;
-    double _containerWdt, _containerHgt;
-    return BlocConsumer<PredictionPageBloc, PredictionPageState>(
-      listener: (context, state) {
-        final bloc = context.read<PredictionPageBloc>();
-        if (state.status == BlocStatus.success) {
-          bloc.add(
-            PredictionPageLoaded(
-              predictionModel: predictionModel,
-            ),
-          );
-        }
-      },
-      buildWhen: (previous, current) => current.status == BlocStatus.success,
-      builder: (context, state) {
-        if (_width > 1160 && _height > 660) {
-          _containerHgt = _height;
-          _containerWdt = _width;
-          return SizedBox(
-            height: _containerHgt,
-            width: _containerWdt,
-            child: Center(
-              child: SizedBox(
-                width: _width * 0.9,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    GraphSide(
-                      predictionModel: predictionModel,
-                    ),
-                    PromptDetails(
-                      predictionModel: predictionModel,
-                    ),
-                  ],
-                ),
+    return BlocProvider(
+      create: (context) => PredictionPageBloc(
+        walletRepository: context.read<WalletRepository>(),
+        eventMarketRepository: context.read<EventMarketRepository>(),
+        streamAppDataChangesUseCase:
+            context.read<StreamAppDataChangesUseCase>(),
+        predictionAddressRepository:
+            context.read<PredictionAddressRepository>(),
+        predictionModelId: predictionModel.id.toString(),
+      ),
+      child: BlocListener<PredictionPageBloc, PredictionPageState>(
+        listener: (context, state) {
+          final bloc = context.read<PredictionPageBloc>();
+          if (state.status == BlocStatus.success) {
+            bloc.add(
+              PredictionPageLoaded(
+                predictionModel: predictionModel,
               ),
-            ),
-          );
-        }
-
-        _containerHgt = (_height * 0.90) - AppBar().preferredSize.height;
-        _containerWdt = _width * 0.95;
-        return Container(
-          margin: EdgeInsets.only(top: AppBar().preferredSize.height + 10),
-          child: Wrap(
-            alignment: WrapAlignment.center,
-            clipBehavior: Clip.hardEdge,
-            children: [
-              SizedBox(
-                height: _containerHgt,
-                child: SingleChildScrollView(
-                  padding: const EdgeInsets.only(bottom: 50),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: _containerWdt,
-                        child: GraphSide(
-                          predictionModel: predictionModel,
-                        ),
-                      ),
-                      SizedBox(
-                        width: _containerWdt,
-                        child: PromptDetails(
-                          predictionModel: predictionModel,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        );
-      },
+            );
+          }
+        },
+        child: BlocBuilder<PredictionPageBloc, PredictionPageState>(
+          buildWhen: (previous, current) => previous.stats != current.stats,
+          builder: (context, state) {
+            final chartStats = state.stats;
+            return PredictionPageWebView(
+              predictionModel: predictionModel,
+              chartStats: chartStats,
+            );
+          },
+        ),
+      ),
     );
   }
 }
