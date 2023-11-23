eec0a4e31388b9e4f8ce6579d0e103b14db6b2fa
Kevin Kamto <kamtokevin@gmail.com>
2023-11-23 15:09:31 -0700

    feat: basic setup of yes/no graphs

---
+++ lib/predict/widgets/prediction_page_yes_graph.dart
@@ -0,0 +1,57 @@
+import 'package:ax_dapp/util/chart/extensions/graph_data.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class PredictionPageYesGraph extends StatelessWidget {
+  const PredictionPageYesGraph({
+    super.key,
+    required this.chartStats,
+    required TooltipBehavior longToolTipBehavior,
+    required ZoomPanBehavior zoomPanBehavior,
+  })  : _longToolTipBehavior = longToolTipBehavior,
+        _zoomPanBehavior = zoomPanBehavior;
+
+  final List<GraphData> chartStats;
+  final TooltipBehavior _longToolTipBehavior;
+  final ZoomPanBehavior _zoomPanBehavior;
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      tooltipBehavior: _longToolTipBehavior,
+      legend: Legend(
+        isVisible: true,
+        position: LegendPosition.bottom,
+      ),
+      zoomPanBehavior: _zoomPanBehavior,
+      series: [
+        FastLineSeries<GraphData, DateTime>(
+          name: 'Yes Token',
+          dataSource: chartStats,
+          xValueMapper: (
+            GraphData data,
+            _,
+          ) =>
+              data.date,
+          yValueMapper: (
+            GraphData data,
+            _,
+          ) =>
+              data.price,
+          dataLabelSettings: const DataLabelSettings(
+            textStyle: TextStyle(
+              fontSize: 10,
+              color: Colors.white,
+            ),
+          ),
+          enableTooltip: true,
+          color: Colors.orange,
+          width: 2,
+          opacity: 1,
+          dashArray: <double>[5, 5],
+        ),
+      ],
+    );
+  }
+}
--- lib/predict/widgets/widgets.dart
+++ lib/predict/widgets/widgets.dart
@@ -3,6 +3,8 @@ export 'desktop_prediction_card.dart';
 export 'markets_filter_desktop.dart';
 export 'markets_filter_mobile.dart';
 export 'prediction_loading_status.dart';
+export 'prediction_page_no_graph.dart';
+export 'prediction_page_yes_graph.dart';
 export 'prediction_prompt.dart';
 export 'probability.dart';
 export 'view_button.dart';
