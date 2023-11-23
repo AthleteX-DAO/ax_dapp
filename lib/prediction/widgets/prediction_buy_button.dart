98210c8c8f1025dd28adeeda418ae8b807e33a30
Kevin Kamto <kamtokevin@gmail.com>
2023-11-23 15:10:21 -0700

    deps: removed unnecessary deps

--- lib/prediction/widgets/prediction_buy_button.dart
+++ lib/prediction/widgets/prediction_buy_button.dart
@@ -6,7 +6,6 @@ import 'package:ax_dapp/repositories/subgraph/usecases/get_buy_info_use_case.dar
 import 'package:ax_dapp/service/controller/swap/swap_repository.dart';
 import 'package:ax_dapp/service/controller/usecases/get_total_token_balance_use_case.dart';
 import 'package:ax_dapp/service/custom_styles.dart';
-import 'package:ax_dapp/service/custom_styles.dart';
 import 'package:ax_dapp/util/toast_extensions.dart';
 import 'package:ax_dapp/wallet/bloc/wallet_bloc.dart';
 import 'package:flutter/material.dart';
